a2ccc2e44afd72d0f295ec7105d9c34c8bd5704b
Mark Sherstan <mshersta@ualberta.ca>
2018-11-03 22:34:00 -0600

    Cleaned up code

--- ILC/Testing/encoder/encoder.ino
+++ ILC/Testing/encoder/encoder.ino
@@ -5,18 +5,22 @@ int counter = 0;
 int aState;
 int aLastState;
 
+
 void setup() {
+  Serial.begin(9600);
+
   pinMode(outputA,INPUT);
   pinMode(outputB,INPUT);
 
-  Serial.begin (9600);
   // Reads the initial state of the outputA
   aLastState = digitalRead(outputA);
 }
 
+
 void loop() {
    aState = digitalRead(outputA); // Reads the "current" state of the outputA
    // If the previous and the current state of the outputA are different, that means a Pulse has occured
+
    if (aState != aLastState){
      // If the outputB state is different to the outputA state, that means the encoder is rotating clockwise
      if (digitalRead(outputB) != aState) {
@@ -27,5 +31,6 @@ void loop() {
      Serial.print("Position: ");
      Serial.println(counter);
    }
-   aLastState = aState; // Updates the previous state of the outputA with the current state
+   // Updates the previous state of the outputA with the current state
+   aLastState = aState;
 }
