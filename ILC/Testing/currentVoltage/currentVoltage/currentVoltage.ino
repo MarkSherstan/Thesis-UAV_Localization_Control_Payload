f4a54484b317637b66ef724b77824524eecf584b
Mark Sherstan <mshersta@ualberta.ca>
2018-11-03 21:53:46 -0600

    Clean up code and simplify conversions

--- ILC/Testing/currentVoltage/currentVoltage/currentVoltage.ino
+++ ILC/Testing/currentVoltage/currentVoltage/currentVoltage.ino
@@ -1,35 +1,28 @@
+// Measuring Current Using ACS712 - 5A model
+
 #include <Servo.h>
 Servo myservo;
 
 
-// Measuring Current Using ACS712
+int scale = 185;  // mV/A
+int offSet = 2500; // mV
+int rawValue;
+double voltage;
+double amps;
 
-const int analogIn = A0;
-int mVperAmp = 185;
-int RawValue= 0;
-int ACSoffset = 2500; 
-double Voltage = 0;
-double Amps = 0;
 
-void setup(){ 
+void setup(){
  Serial.begin(9600);
-   myservo.attach(3);
-  myservo.writeMicroseconds(1000);
+
+ myservo.attach(3);
+ myservo.writeMicroseconds(1000);
 }
 
+
 void loop(){
- 
- RawValue = analogRead(analogIn);
- Voltage = (RawValue / 1024.0) * 5000; // Gets you mV
- Amps = ((Voltage - ACSoffset) / mVperAmp);
- 
- 
-// Serial.print("Raw Value = " ); // shows pre-scaled value 
-// Serial.print(RawValue); 
-// Serial.print("\t mV = "); // shows the voltage measured 
-// Serial.print(Voltage,3); // the '3' after voltage allows you to display 3 digits after decimal point
-// Serial.print("\t Amps = "); // shows the voltage measured 
- Serial.println(Amps*1000.0,3); // the '3' after voltage allows you to display 3 digits after decimal point
- delay(10); 
- 
+ rawValue = analogRead(A0);
+ amps = (((RawValue / 1024.0) * 5000) - offSet) / scale;
+
+ Serial.println(amps,3);
+ delay(10);
 }
