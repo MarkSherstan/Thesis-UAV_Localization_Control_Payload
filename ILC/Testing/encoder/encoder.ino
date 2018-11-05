dcf010d709ab8df4a17c5ada5e7a13c04dcff2ac
Mark Sherstan <mshersta@ualberta.ca>
2018-11-04 18:06:45 -0700

    New sketch... Use interrupts

--- ILC/Testing/encoder/encoder.ino
+++ ILC/Testing/encoder/encoder.ino
@@ -1,47 +1,47 @@
-#define outputA 6
-#define outputB 7
+//From bildr article: http://bildr.org/2012/08/rotary-encoder-arduino/
+// https://www.arduino.cc/en/Reference/AttachInterrupt?setlang=it
 
-int counter = 0;
-int angle = 0;
-int aState;
-int aLastState;
 
+//these pins can not be changed 2/3 are special pins
+int encoderPin1 = 2;
+int encoderPin2 = 3;
+
+volatile int lastEncoded = 0;
+volatile long encoderValue = 0;
+
+long lastencoderValue = 0;
+
+int lastMSB = 0;
+int lastLSB = 0;
 
 void setup() {
-  Serial.begin(9600);
+  Serial.begin (9600);
 
-  pinMode(outputA,INPUT);
-  pinMode(outputB,INPUT);
+  pinMode(encoderPin1, INPUT);
+  pinMode(encoderPin2, INPUT);
 
-  // Reads the initial state of the outputA
-  aLastState = digitalRead(outputA);
+  digitalWrite(encoderPin1, HIGH); //turn pullup resistor on
+  digitalWrite(encoderPin2, HIGH); //turn pullup resistor on
+
+  //call updateEncoder() when any high/low changed seen
+  //on interrupt 0 (pin 2), or interrupt 1 (pin 3)
+  attachInterrupt(0, updateEncoder, CHANGE);
+  attachInterrupt(1, updateEncoder, CHANGE);
 }
 
+void loop(){
+  //Do stuff here
+  Serial.println(encoderValue);
+  //delay(1000); //just here to slow down the output, and show it will work even during a delay
+}
 
-void loop() {
-   aState = digitalRead(outputA); // Reads the "current" state of the outputA
-   // If the previous and the current state of the outputA are different, that means a Pulse has occured
+void updateEncoder(){
+  int MSB = digitalRead(encoderPin1); //MSB = most significant bit
+  int LSB = digitalRead(encoderPin2); //LSB = least significant bit
 
-   if (aState != aLastState){
-     // If the outputB state is different to the outputA state, that means the encoder is rotating clockwise
-     if (digitalRead(outputB) != aState) {
-       counter ++;
-     } else {
-       counter --;
-     }
-
-     // Track total rotations and bound between -20 and 20
-     if (counter >= 40){
-       counter = 0;
-     } else if (counter <= -40){
-       counter = 0;
-     }
-
-     // Print angle to screen. 360 / 40 = 9
-     //Serial.println(counter);
-     Serial.println(counter*9);
-   }
-   
-   // Updates the previous state of the outputA with the current state
-   aLastState = aState;
+  int encoded = (MSB << 1) |LSB; //converting the 2 pin value to single number
+  int sum = (lastEncoded << 2) | encoded; //adding it to the previous encoded value
+  if(sum == 0b1101 || sum == 0b0100 || sum == 0b0010 || sum == 0b1011) encoderValue ++;
+  if(sum == 0b1110 || sum == 0b0111 || sum == 0b0001 || sum == 0b1000) encoderValue --;
+  lastEncoded = encoded; //store this value for next time
 }
