cdbf6cd7fc0c43d89a975a2839c77b3328487184
Mark Sherstan <mshersta@ualberta.ca>
2018-11-04 21:20:53 -0700

    Changed order of code and added longer delay.

--- ILC/Testing/voltageCurrentPower/voltageCurrentPower.ino
+++ ILC/Testing/voltageCurrentPower/voltageCurrentPower.ino
@@ -5,15 +5,20 @@
 Adafruit_INA219 ina219;
 Servo myservo;
 
-uint32_t currentFrequency;
-float shuntvoltage = 0;
-float busvoltage = 0;
-float current_mA = 0;
-float loadvoltage = 0;
-float power_mW = 0;
+// Adafruit_INA219 ina219_A;
+// Adafruit_INA219 ina219_B(0x41);
+
+  float shuntvoltage = 0;
+  float busvoltage = 0;
+  float current_mA = 0;
+  float loadvoltage = 0;
+  float power_mW = 0;
+
+  uint32_t currentFrequency;
 
 void setup(void){
   Serial.begin(115200);
+
 
   myservo.attach(3);
   myservo.writeMicroseconds(1520);
@@ -22,21 +27,21 @@ void setup(void){
   //ina219.setCalibration_32V_1A(); // 32V, 1A range
   //ina219.setCalibration_16V_400mA(); // 16V, 400mA range
 
-  myservo.writeMicroseconds(1000);
+  //myservo.writeMicroseconds(1000);
 }
 
 void loop(void){
+
+
   shuntvoltage = ina219.getShuntVoltage_mV();
   busvoltage = ina219.getBusVoltage_V();
   current_mA = ina219.getCurrent_mA();
   power_mW = ina219.getPower_mW();
   loadvoltage = busvoltage + (shuntvoltage / 1000);
 
-  //Serial.print(busvoltage); Serial.println(" V");
-  //Serial.print(shuntvoltage); Serial.println(" mV");
-  Serial.print(loadvoltage); Serial.print(",");
-  Serial.print(current_mA); Serial.print(",");
-  Serial.println(power_mW);
+  //Serial.print(loadvoltage); Serial.print(",");
+  Serial.println(current_mA); //Serial.print(",");
+  //Serial.println(power_mW/1000);
 
-  delay(1);
+  delay(10);
 }
