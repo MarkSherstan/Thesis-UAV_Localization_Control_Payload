ddee2c511032981440cc1a6648a1499f503ab1e3
MarkSherstan <mshersta@ualberta.ca>
2018-11-26 23:36:01 -0700

    Get new branch up to date.

--- ILC/Data/fileReader.m
+++ ILC/Data/fileReader.m
@@ -1,3 +1,8 @@
+fieldnames(C)
+SYS = pid(C.Kp,C.Ki,C.Kd,C.Tf,Ts)
+
+
+% https://www.mathworks.com/videos/control-design-with-matlab-and-simulink-90419.html
 clear all
 close all
 clc
@@ -31,14 +36,22 @@ u = [zeros(1,length(0:Ts:time(1))) PWM']';
 % figure; plot(t,y); title('y')
 % figure; plot(t,u); title('u')
 
-y = y(1:1800);
-u = u(1:1800);
 
-data = iddata(y,u,Ts);
-sys = tfest(data,3);
+% t = t(1:1800);
+% y = y(1:1800); y(621:end) = 263.9360;
+% u = u(1:1800);
 
-sys.num
-sys.den
+figure; plot(t,u,t,y); legend('Input-PWM','Output-Current');
+%figure; plot(y)
+
+inputu = u;
+outputy = y;
+
+% data = iddata(y,u,Ts);
+% sys = tfest(data,3);
+% 
+% sys.num
+% sys.den
 
 
 % % Make some figures
