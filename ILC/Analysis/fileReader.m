9007ccb5eda8840142ff1895e7f28e671ecb3e1d
MarkSherstan <mshersta@ualberta.ca>
2018-11-26 23:59:14 -0700

    Turn fileReader into a function called by main.

--- ILC/Analysis/fileReader.m
+++ ILC/Analysis/fileReader.m
@@ -1,16 +1,12 @@
-fieldnames(C)
-SYS = pid(C.Kp,C.Ki,C.Kd,C.Tf,Ts)
+function [Ts U Y] = fileReader(fileName)
 
-
-% https://www.mathworks.com/videos/control-design-with-matlab-and-simulink-90419.html
-clear all
-close all
-clc
-
-load 1000PWM.txt
+% Load file from different directory
+fileNameAndPath = strcat('../Data/',fileName,'.txt');
+load(fileNameAndPath)
 A = X1000PWM;
 
-time = A(:,1) * 1e-6;
+% Simplify data loaded in
+timeSec = A(:,1) * 1e-6;
 pos = A(:,2); % Measured at gear (multiply by 2 to get pinion)
 voltage = A(:,3);
 current = A(:,4);
@@ -19,50 +15,22 @@ PWM = A(:,5);
 % Find the min angle values for rpm calc
 localMin = islocalmin(pos,'MinProminence',30);
 idx = find(localMin);
-deltaTime = time(idx);
+deltatimeSec = timeSec(idx);
 
-for ii = 1:length(deltaTime) - 1
-    rps(ii) = deltaTime(ii+1)-deltaTime(ii);
+for ii = 1:length(deltatimeSec) - 1
+    rps(ii) = deltatimeSec(ii+1)-deltatimeSec(ii);
 end
 
 rpm = (1/mean(rps))*60;
 
-%Find transfer function
-Ts = mean(diff(time));
-t = [0:Ts:time(1) time']';
-y = [ones(1,length(0:Ts:time(1)))*current(1) current']';
-u = [zeros(1,length(0:Ts:time(1))) PWM']';
+% Find sample rate and add intial values
+Ts = mean(diff(timeSec));
+t = [0:Ts:timeSec(1) timeSec']';
+U = [zeros(1,length(0:Ts:timeSec(1))) PWM']';
+Y = [ones(1,length(0:Ts:timeSec(1)))*current(1) current']';
 
-% figure; plot(t,y); title('y')
-% figure; plot(t,u); title('u')
-
-
-% t = t(1:1800);
-% y = y(1:1800); y(621:end) = 263.9360;
-% u = u(1:1800);
-
-figure; plot(t,u,t,y); legend('Input-PWM','Output-Current');
-%figure; plot(y)
-
-inputu = u;
-outputy = y;
-
-% data = iddata(y,u,Ts);
-% sys = tfest(data,3);
-%
-% sys.num
-% sys.den
-
-
-% % Make some figures
-% figure(1)
-% plot(time,voltage)
-% title('Voltage vs Time')
-%
-% figure(2)
-% plot(time,current)
-% title('Current vs Time')
-%
-% figure(3)
-% plot(time,pos,time(idx),pos(idx),'*r')
-% title('Position vs time')
+% Plot the input and output
+figure
+plot(t,U,t,Y)
+legend('Input-PWM','Output-Current');
+title('Single Input Single Output Response(s)')
