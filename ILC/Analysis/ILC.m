a585b27924505083b615e05c92742a1da9df56a5
MarkSherstan <mshersta@ualberta.ca>
2018-11-28 10:07:45 -0700

    Remove G0 matrix and clean up G matrix formulation

--- ILC/Analysis/ILC.m
+++ ILC/Analysis/ILC.m
@@ -13,54 +13,28 @@ lsim(sys,U,[],x0)
 ylabel('Amplitude')
 xlabel('time [s]')
 
-% Set time period, n0, r and N
+% Set time range t, pure time delay n0, relative degree r, and matrix sizes N
 t = 0:Ts:10;
 n0 = 0;
 r = 1;
 N = length(t);
 
-% Assume G0 goes to 0
-
-% % Formulate G0
-% G0 = zeros(N,1);
-% rvec = ((n0+r):N)';
-%
-% %G0(2,1) = Cd' * Ad^(n0+r+1);
-%
-% for ii = 1:length(rvec)
-%   Apow_vec = Ad^rvec(ii);
-%   %G0(ii) = Cd*Apow_vec;
-% end
+% G0 not formulated as initial condition is 0
 
 % Formulate G
 Gvec = zeros(N,1);
-% Gvec(1,1) = Cd' * Ad^(r-1) * Bd;
-% Gvec(2,1) = Cd' * Ad^(r) * Bd;
 rvec = ((r-1):(N-n0-1))';
 
 for ii = 1:length(rvec)
-  Apow_vec = Ad^rvec(ii);
-  Gvec(ii) = Cd*Apow_vec*Bd;
+  ApowVec = Ad^rvec(ii);
+  Gvec(ii) = Cd*ApowVec*Bd;
 end
-
-% for ii = 3:length(Gvec)
-%     Gvec(ii,1) = Cd' * Ad^(ii-n0-1) * Bd;
-% end
 
 G = tril(toeplitz(Gvec));
 
-
-% Formulate U to be 1x101, solve, and plot
-%U = [rr 0]';
-U = U';
-
-y = G*U; % y = G0*x0 + G*U;
-
-%%%%%%%%%%%%%%%%%%%%%%%
-% Part F
-%%%%%%%%%%%%%%%%%%%%%%%
+% Set up ILC
 figure(2)
-Rj = U;
+Rj = U';
 
 jmax = 150;
 l0 = 0.95;
@@ -77,14 +51,15 @@ yyy = zeros(jmax,N);
 
 for ii = 1:jmax
     Uj = Q*Ujold + L*Ejold;
-    Yj = G*Uj; %Yj = G*Uj + G0*x0;
+    Yj = G*Uj;
 
     Ej = Rj - Yj; Ej(1) = 0;
     Ejold = Ej;
     Ujold = Uj;
 
     % To visualize response real time
-    %plot(t,Ej,t,Rj); title(['Iteration: ', num2str(ii)]); pause(0.1);
+    plot(t,Ej,t,Rj); title(['Iteration: ', num2str(ii)]); pause(0.1);
+
     yyy(ii,:) = Ej;
     e2k(ii) = Ej' * Ej;
 end
