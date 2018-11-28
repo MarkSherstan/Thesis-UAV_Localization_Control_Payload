4aac6f6f7d4de6310150b31933a624873e29ff3f
MarkSherstan <mshersta@ualberta.ca>
2018-11-27 23:04:25 -0700

    Add plot and prepare for stability analysis

--- ILC/Analysis/main.m
+++ ILC/Analysis/main.m
@@ -5,6 +5,47 @@ close all
 fileName = '1000PWM';
 [Ts t U Y] = fileReader(fileName);
 
+% Transfer function and coeffcients from system identification
+num = [5.0861   27.1602];
+den = [1.0000   31.9422  101.6955];
+tf1 = tf(num,den);
+
+% Load state space model from simscape linearization
+load('simscapeLinearModelSS.mat')
+
+% Plot the results of the system identification
+figure
+hold on
+  plot(t,U,'-b',t,Y,'-r','LineWidth',1)
+  Ytf = 1000*step(tf1,5);
+  plot(t(533:length(Ytf)+532),Ytf,'-k','LineWidth',2)
+  legend('Input-PWM','Output-Current (mA)','Linear Transfer Function','FontSize',16,'Location','SouthEast');
+  title('Single Input - Single Output Response(s)','FontSize',16)
+  xlabel('Time (s)','FontSize',16)
+  ylim([0 1050])
+  xlim([0 10])
+hold off
+
+% Stability analysis
+figure; pzmap(num,den);
+roots(den);
+roots(num);
+figure; step(num,den);
+figure; bode(tf1);
+
+
+% Implment ILC
+%ILC(tf1,Ts)
+
+% Estimation result(s):
+% RatedV = 9.6297;
+% armatureInduc = 7.4126e-06;
+% noLoadCurrent = 0.48351;
+% noLoadSpeed = 27.2;
+% rotorInertia = 9.8946e-06;
+% stallTorque = 0.68957;
+% supply4noLoadCurrent = 21.841;
+
 % % Original Estimates of motor parameters and validation results
 %
 % armatureInduc = 12e-6;
@@ -16,28 +57,3 @@ fileName = '1000PWM';
 % rotorInertia = 0.0002;
 %
 % validation = [t Y./1000];
-
-% Estimation result(s):
-RatedV = 9.6297;
-armatureInduc = 7.4126e-06;
-noLoadCurrent = 0.48351;
-noLoadSpeed = 27.2;
-rotorInertia = 9.8946e-06;
-stallTorque = 0.68957;
-supply4noLoadCurrent = 21.841;
-
-% Transfer function and coeffcients from system identification
-num = [5.0861   27.1602];
-den = [1.0000   31.9422  101.6955];
-
-tf1 = tf(num,den);
-
-% Stability analysis
-pzmap(num,den)
-roots(den) % Find poles
-roots(num)
-step(num,den)
-bode(tf1)
-
-% Implment ILC
-ILC(tf1,Ts)
