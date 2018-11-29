347f66b5a4b7a57869997598bece0aa416533b0f
Mark Sherstan <mshersta@ualberta.ca>
2018-11-28 19:07:12 -0700

    Added periodic disturbances from gear train

--- ILC/Analysis/ILC.m
+++ ILC/Analysis/ILC.m
@@ -5,7 +5,7 @@ function [ ] = ILC(sys,Ts)
 
 % Set initial condition x0, time range t, pure time delay n0, relative degree r, and matrix sizes N
 x0 = 0;
-t = 0:Ts:10;
+t = 0:Ts:17.5;
 n0 = 0;
 r = 1;
 N = length(t);
@@ -27,19 +27,37 @@ end
 
 G = tril(toeplitz(Gvec));
 
+% Define distrubance every 2 rotations or every 3 seconds after t = 1.5s
+disturbance = zeros(N,1);
+counter = 1;
+
+for ii = 267:length(disturbance)
+  if (counter > 267) & (counter < 267+20)
+    disturbance(ii,1) = 50;
+  elseif counter == 288;
+    counter = 1;
+  end
+counter = counter + 1;
+end
+
+% Add a big distrubance after 1 full rotation at about 10.8 s after t = 1.5s
+disturbance(1935:1935+125) = disturbance(1935:1935+125)+125;
+
+% Plot the disturbance
+figure(1);
+plot(t,disturbance,'-k','LineWidth',1.5)
+xlabel('Time (s)','FontSize',16)
+ylabel('Disturbance (mA)','FontSize',16)
+title('Periodic Disturbance due to Gear Train')
+
 % Set up ILC
-jmax = 50;
+jmax = 25;
 l0 = 0.95;
 q0 = 1;
 
 L = l0 * eye(N,N);
 Q = q0 * eye(N,N);
 I = eye(N);
-
-noise = 20*rand(N,1);
-disturbance = [zeros(1,1000) 100*ones(1,73) zeros(1,700)]';
-
-figure(1); plot(disturbance)
 
 Uj = zeros(N,1); Ujold = Uj;
 Ej = zeros(N,1); Ejold = Ej;
@@ -51,14 +69,15 @@ yyy = zeros(jmax,N);
 figure(2)
 
 for ii = 1:jmax
+
+    noise = 15*rand(N,1) - 7.5;
+
     Uj = Q*Ujold + L*Ejold;
     Yj = G*Uj - (I - G)*(noise + disturbance);
 
     Ej = Rj - Yj; Ej(1) = 0;
     Ejold = Ej;
     Ujold = Uj;
-
-    noise = 20*rand(N,1);
 
     % To visualize response real time
     subplot(1,3,1); plot(t,Ej,t,Rj); title(['Ej Iteration: ', num2str(ii)]); ylim([-50 600]); pause(0.1);
@@ -69,21 +88,13 @@ for ii = 1:jmax
     e2k(ii) = Ej' * Ej;
 end
 
-% figure(3)
-% plot(t,Rj,t,Yj,t,Uj)
-% title('ILC - Supervector Format - Iteration 55')
-% xlabel('Time (k) [s]')
-% ylabel('Amplitude')
-% legend('r_k','y_k','u_k')
-
 % figure(4)
 % semilogy(1:length(e2k),e2k)
 % title('Error as a function of Iteration Index - Semi-log Scale Plot')
 % xlabel('Iteration Index')
 % ylabel('Sum of Squares of Error')
 
-
-figure(5)
+figure(3)
 y = (1:jmax)';
 x = t';
 z = yyy;
