b2d65a324625776cbc37b4b80abe9845ec03f404
MarkSherstan <mshersta@ualberta.ca>
2018-11-30 16:59:48 -0700

    Commit initial file for non linear

---
+++ ILC/Modelling/nonLinearSim.m
@@ -0,0 +1,106 @@
+
+% Get state space - system already in discrete time
+[Ad Bd Cd Dd] = ssdata(sysd2);
+
+% Set initial condition x0, time range t, pure time delay n0, relative degree r, and matrix sizes N
+x0 = 0;
+t = 0:Ts:17.5;
+n0 = 0;
+r = 1;
+N = length(t);
+
+% Define input vector U and reference J
+U = [zeros(1,267) 1000*ones(1,N-267)];
+Rj = [zeros(1,267) 263.9*ones(1,N-267)]';
+
+% G0 not formulated as initial condition is 0
+
+% Formulate G
+Gvec = zeros(N,1);
+rvec = ((r-1):(N-n0-1))';
+
+for ii = 1:length(rvec)
+  ApowVec = Ad^rvec(ii);
+  Gvec(ii) = Cd*ApowVec*Bd;
+end
+
+G = tril(toeplitz(Gvec));
+
+% Define distrubance every 2 rotations or every 3 seconds after t = 1.5s
+disturbance = zeros(N,1);
+counter = 1;
+
+for ii = 267:length(disturbance)
+  if (counter > 534) & (counter < 534+20)
+    disturbance(ii,1) = 50;
+  elseif counter == 555;
+    counter = 1;
+  end
+counter = counter + 1;
+end
+
+% Add a big distrubance after 1 full rotation at about 10.8 s after t = 1.5s
+disturbance(2177:2177+150) = 125;
+
+
+% Set up ILC
+jmax = 4;
+l0 = 0.95;
+q0 = 1;
+
+L = l0 * eye(N,N);
+Q = q0 * eye(N,N);
+I = eye(N);
+
+Uj = zeros(N,1); Ujold = U';
+Ej = zeros(N,1); Ejold = Ej;
+
+e2k = zeros(jmax,1);
+EE = zeros(jmax,N);
+
+% Set up video writer
+v = VideoWriter('simulation.avi');
+v.FrameRate = 5;
+open(v);
+
+figure(2)
+
+
+Rj = [zeros(1,267) 263.9*ones(1,N-267)]';
+U = [[0:Ts:17.5]' [zeros(1,267) 1000*ones(1,N-267)]'];
+
+
+
+
+
+
+% Run ILC and plot the response for each iteration
+for ii = 1:jmax
+  U = Uj;
+  sim('Twist',17.5)
+  Yj = Y;
+
+  noise = 15*rand(N,1) - 7.5;
+
+  Uj = Q*Ujold + L*Ejold;
+  %Yj = G*Uj - (I-G)*(noise + disturbance);
+
+  Ej = Rj - Yj; Ej(1) = 0;
+  Ejold = Ej;
+  Ujold = Uj;
+
+  plot(t,Yj,t,Rj,'-k','LineWidth',1.5);
+  title('Output, Yj','FontSize',16);
+  ylabel('Output Response (mA)','FontSize',16);
+  ylim([-25 300]);
+  xlim([0 17.5])
+
+  frame = getframe(gcf);
+  writeVideo(v,frame);
+  pause(0.1);
+
+  EE(ii,:) = Ej;
+  e2k(ii) = Ej'*Ej;
+end
+
+close(v);
