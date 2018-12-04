aa46e74cdefa7406030c5d7b42eb123d9af46a2a
MarkSherstan <mshersta@ualberta.ca>
2018-12-04 10:49:25 -0700

    Add comments, simplify plots, and load in new .mat file

--- ILC/Analysis/main.m
+++ ILC/Analysis/main.m
@@ -1,33 +1,26 @@
 clear all
 close all
 
-% Get the data
+% Get the experimental data and create a plot. Output Ts and data to make the plot
 fileName = '1000PWM';
 [Ts t U Y] = fileReader(fileName);
 
-% Transfer function and coeffcients from system identification
-num = [5.0861   27.1602];
-den = [1.0000   31.9422  101.6955];
-tf1 = tf(num,den);
-
-% Load state space model from simscape linearization
-load('simscapeLinearModelSS.mat')
-
-% Plot the results of the system identification
-figure
+figure(1)
 hold on
   plot(t,U,'-b',t,Y,'-r','LineWidth',1)
-  Ytf = 1000*step(tf1,5);
-  plot(t(533:length(Ytf)+532),Ytf,'-k','LineWidth',2)
-  legend('Input-PWM','Output-Current (mA)','Linear Transfer Function','FontSize',16,'Location','SouthEast');
-  title('Single Input - Single Output Response(s)','FontSize',16)
+  legend('Input-PWM','Output-Current (mA)','FontSize',16,'Location','SouthEast');
+  title('Single Input - Single Output Response','FontSize',16)
   xlabel('Time (s)','FontSize',16)
   ylim([0 1050])
   xlim([0 10])
 hold off
 
+% Load the transfer function from system identification (tf1), the state space
+% model from simscape linearization (ss2), and the paramter estimation values
+% for the DC motor (non linear simulation)
+load('model_tf_ss_motor_data.mat')
+
 % Stability analysis
-[sysd1 sysd2] = stability(tf1, linsys1, Ts);
+[sysd1 sysd2] = stability(tf1, ss2, Ts);
 
 % Implment ILC
-%ILC(sysd1,Ts)
