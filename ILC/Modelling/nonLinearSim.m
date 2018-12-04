fa9da7c35dce499eae95610655889f4cb2f6cd1e
MarkSherstan <mshersta@ualberta.ca>
2018-12-03 23:56:08 -0700

    Removed unused code and fixed back to original ILC sim values

--- ILC/Modelling/nonLinearSim.m
+++ ILC/Modelling/nonLinearSim.m
@@ -1,8 +1,4 @@
-% function [ ] = nonLinearSim(sys,Ts)
-close all
-
-
-% Parameter estimation fit results for DC motor
+% Parameter estimation fit results for DC motor --> Add this to a single .mat file?
 RatedV = 9.6297;
 armatureInduc = 7.4126e-06;
 noLoadCurrent = 0.48351;
@@ -14,9 +10,6 @@ supply4noLoadCurrent = 21.841;
 % Get state space - system already in discrete time
 [Ad Bd Cd Dd] = ssdata(sysd2);
 
-%Ts = round(Ts,5);
-Ts = 0.01;
-
 % Set initial condition x0, time range t, pure time delay n0, relative degree r, and matrix sizes N
 x0 = 0;
 t = 0:Ts:17.5;
@@ -25,10 +18,10 @@ r = 1;
 N = length(t);
 
 % % Define input vector U and reference J
-Uref = [zeros(1,151) 1000*ones(1,N-151)];
-Rj = [zeros(1,152) 265.1*ones(1,N-152)]'; %263.9
+Uref = [zeros(1,267) 1000*ones(1,N-267)];
+Rj = [zeros(1,267) 263.9*ones(1,N-267)]';
 
-% G0 not formulated as initial condition is 0
+% G0 not formulated as initial conditions are 0
 
 % Formulate G
 Gvec = zeros(N,1);
@@ -45,17 +38,17 @@ G = tril(toeplitz(Gvec));
 disturbance = zeros(N,1);
 counter = 1;
 
-for ii = 150:length(disturbance)
-  if (counter > 300) & (counter < 300+20)
+for ii = 267:length(disturbance)
+  if (counter > 534) & (counter < 534+20)
     disturbance(ii,1) = 50;
-  elseif counter == 321;
+  elseif counter == 555;
     counter = 1;
   end
 counter = counter + 1;
 end
 
 % Add a big distrubance after 1 full rotation at about 10.8 s after t = 1.5s
-disturbance(1080:1080+150) = 125;
+disturbance(2177:2177+150) = 125;
 
 % Set up ILC
 jmax = 25;
@@ -69,17 +62,7 @@ I = eye(N);
 Uj = zeros(N,1); Ujold = Uref';
 Ej = zeros(N,1); Ejold = Ej;
 
-e2k = zeros(jmax,1);
-EE = zeros(jmax,N);
-
-% Set up video writer
-v = VideoWriter('simulation.avi');
-v.FrameRate = 5;
-open(v);
-
 figure(2)
-
-% Uj = Ujold;
 
 % Run ILC and plot the response for each iteration
 for ii = 1:jmax
@@ -95,13 +78,4 @@ for ii = 1:jmax
   Ujold = Uj;
 
   plotter(ii,t,Ej,Yj,Uj,Rj,Uref)
-  frame = getframe(gcf);
-  writeVideo(v,frame);
-
-  % EE(ii,:) = Ej;
-  % e2k(ii) = Ej'*Ej;
 end
-
-
-
-close(v);
