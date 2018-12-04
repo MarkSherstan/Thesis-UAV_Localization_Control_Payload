b6e4c798deeafe14fa0cb8c191359be63e2f3648
MarkSherstan <mshersta@ualberta.ca>
2018-12-04 11:20:00 -0700

    Update file names

--- ILC/Analysis/nonLinearSim.m
+++ ILC/Analysis/nonLinearSim.m
@@ -47,7 +47,7 @@ for ii = 1:jmax
   % Requirment for simulink (needs time stamp)
   U = [t' Uj];
 
-  simOut = sim('nonLinearILC');
+  simOut = sim('nonLinearModel');
 
   Yj = Y.Data + disturbance;
 
