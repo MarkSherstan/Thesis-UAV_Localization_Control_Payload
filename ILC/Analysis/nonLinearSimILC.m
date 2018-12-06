9f2720e96fa791238321da91365f3a110fb43ee1
MarkSherstan <mshersta@ualberta.ca>
2018-12-06 11:25:47 -0700

    Remove G matrix portion and add plots

--- ILC/Analysis/nonLinearSimILC.m
+++ ILC/Analysis/nonLinearSimILC.m
@@ -1,34 +1,17 @@
-% Get state space data - system already in discrete time.
-[Ad Bd Cd Dd] = ssdata(sysd2);
-
-% Set initial condition x0, time range t, pure time delay n0, relative degree r,
+% Set time range t, pure time delay n0, relative degree r,
 % and matrix sizes N from length of t.
-x0 = 0;
 t = 0:Ts:17.5;
 n0 = 0;
 r = 1;
 N = length(t);
 
-% Define input vector U and reference J. Step at 1.5s
+% Define input vector U and reference R. Step at 1.5s
 Uref = [zeros(1,267) 1000*ones(1,N-267)];
 Rj = [zeros(1,267) 263.9*ones(1,N-267)]';
 
-% G0 not formulated as initial conditions are 0
-
-% Formulate G
-Gvec = zeros(N,1);
-rvec = ((r-1):(N-n0-1))';
-
-for ii = 1:length(rvec)
-  ApowVec = Ad^rvec(ii);
-  Gvec(ii) = Cd*ApowVec*Bd;
-end
-
-G = tril(toeplitz(Gvec));
-
 % Set up ILC variables
 jmax = 25;
-l0 = 0.95;
+l0 = 0.3;
 q0 = 1;
 
 L = l0 * eye(N,N);
@@ -42,6 +25,8 @@ Ej = zeros(N,1); Ejold = Ej;
 figure(2)
 
 for ii = 1:jmax
+  noise = 15*rand(N,1) - 7.5;
+
   Uj = Q*Ujold + L*Ejold;
 
   % Requirment for simulink (needs time stamp)
@@ -56,4 +41,29 @@ for ii = 1:jmax
   Ujold = Uj;
 
   plotter(ii,t,Ej,Yj,Uj,Rj,Uref)
+
+  e2k(ii) = Ej'*Ej;
 end
+
+% close all
+%
+% figure
+% semilogy(1:length(e2k),e2k)
+% %title('Error as a function of Iteration Index - Semi-log Scale Plot')
+% xlabel('Iteration Index, j','interpreter','latex','FontSize',16)
+% ylabel('2-norm Error $||e(k)^2_2||$','interpreter','latex','FontSize',16)
+% grid on
+%
+% figure
+% subplot(2,1,1);
+% plot(t,Uj,t,Uref,'-k','LineWidth',1.5);
+% title('Input, Uj','FontSize',16);
+% xlabel('Time (s)','FontSize',16);
+% ylabel('Input PWM (\mus)','FontSize',16);
+% ylim([-50 2000]);
+%
+% subplot(2,1,2);
+% plot(t,Yj,t,Rj,'-k','LineWidth',1.5);
+% title('Output, Yj','FontSize',16);
+% xlabel('Time (s)','FontSize',16);
+% ylabel('Output Response (mA)','FontSize',16);
