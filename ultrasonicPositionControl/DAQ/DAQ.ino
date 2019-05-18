4d0b184f16ffdf93230ea3248ed30eeb55735519
Mark Sherstan <mshersta@ualberta.ca>
2019-05-18 10:33:20 -0600

    Commit sketch for 3DOF of sensors.

--- ultrasonicPositionControl/DAQ/DAQ.ino
+++ ultrasonicPositionControl/DAQ/DAQ.ino
@@ -1,8 +1,9 @@
 // Define variables
-const int trigPin = 9;
-const int echoPin = 10;
+const int trigPin[] = {7, 9, 11};
+const int echoPin[] = {8, 10, 12};
+int sensorCount = 3;
+int distance[3];
 long duration;
-int distance;
 
 
 void setup() {
@@ -10,35 +11,48 @@ void setup() {
   Serial.begin(9600);
 
   // Setup digital pins input and output
-  pinMode(trigPin, OUTPUT);
-  pinMode(echoPin, INPUT);
+  for(int ii=0; ii<sensorCount; ii++){
+     pinMode(trigPin[ii], OUTPUT);
+     pinMode(echoPin[ii], INPUT);
+  }
 }
 
 
 void loop() {
-  // Clear the trigPin
-  digitalWrite(trigPin, LOW);
+  // Clear the trigPins
+  trigPinState(0);
   delayMicroseconds(3);
 
   // Set the trigPin on HIGH for 10 micro seconds
-  digitalWrite(trigPin, HIGH);
+  trigPinState(1);
   delayMicroseconds(10);
-  digitalWrite(trigPin, LOW);
+  trigPinState(0);
 
   // Read the echoPin, returns the sound wave travel time in microseconds
-  duration = pulseIn(echoPin, HIGH);
-
   // distance = traveltime x speed of sound [cm/us] x 1/2
-  distance = duration * 0.0343 * 0.5;
+  for(int ii=0; ii<sensorCount; ii++){
+    duration = pulseIn(echoPin[ii], HIGH);
+    distance[ii] = duration * 0.0343 * 0.5;
+  }
 
   // Write bytes
-  writeBytes(&distance);
+  writeBytes(&distance[0], &distance[1], &distance[2]);
 }
 
 
-void writeBytes(int* data1){
+void trigPinState(bool* state){
+  // Set the state of the trigger pins
+  for(int ii=0; ii<sensorCount; ii++){
+    digitalWrite(trigPin[ii], state);
+  }
+}
+
+
+void writeBytes(int* data1, int* data2, int* data3){
   // Cast to a byte pointer
   byte* byteData1 = (byte*)(data1);
+  byte* byteData2 = (byte*)(data2);
+  byte* byteData3 = (byte*)(data3);
 
   // Byte array with header for transmission
   byte buf[4] = {0x9F, 0x6E, byteData1[0], byteData1[1]};
