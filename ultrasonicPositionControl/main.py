b981ea653b38f4c0a94db06a7405fb3bff2a4491
Mark Sherstan <mshersta@ualberta.ca>
2019-06-25 13:04:28 -0600

    Initial commit of trajectory generation.

--- ultrasonicPositionControl/main.py
+++ ultrasonicPositionControl/main.py
@@ -329,6 +329,117 @@ class Controller:
 			# Display close message
 			print 'Closing...\n'
 
+	def trajectoryControl(self, vehicle, s):
+		# Initialize variables
+		counter = 0
+		T = 5
+
+		# Set the start position
+		northStart = 1.1
+		eastStart = 0.8
+
+		# Set the desired NED locations
+		self.northDesired = 0.5
+		self.eastDesired = 0.3
+		self.downDesired = -0.4
+
+		# Generate a trajectory that should take T seconds
+		northIC = [northStart, self.northDesired, 0, 0, 0, 0]
+		eastIC = [eastStart, self.eastDesired, 0, 0, 0, 0]
+
+		pN = self.trajectoryGen(northIC, T, self.duration, False)
+		pE = self.trajectoryGen(eastIC, T, self.duration, False)
+
+		# Combined local frame with actual initial position
+		northStart -= vehicle.location.local_frame.north
+		eastStart -= vehicle.location.local_frame.east
+
+		# Start timers
+		self.startTime = time.time()
+		prevTime = time.time()
+
+		# Run for 2.5*T seconds
+		while (time.time() < self.startTime + 2.5*T):
+			# Get current values and transform to trajectory defined start location
+			northCurrentPos = vehicle.location.local_frame.north + northStart # + (random.random()-0.5)*0.1
+			eastCurrentPos = vehicle.location.local_frame.east + eastStart # + (random.random()-0.5)*0.1
+			downCurrentPos = vehicle.location.local_frame.down # + (random.random()-0.5)*0.1
+			timeStamp = time.time() - self.startTime
+
+			# Set the desired position based on time counter index
+			if counter < len(pN):
+				desiredN = pN[counter]
+				desiredE = pE[counter]
+			else:
+				desiredN = self.northDesired
+				desiredE = self.eastDesired
+
+			# Save values for plotting
+			self.northDesiredList.append(desiredN)
+			self.northActualList.append(northCurrentPos)
+			self.eastDesiredList.append(desiredE)
+			self.eastActualList.append(eastCurrentPos)
+			self.downDesiredList.append(self.downDesired)
+			self.downActualList.append(downCurrentPos)
+			self.timeList.append(timeStamp)
+
+			# Error caculations
+			errorNorth = desiredN - northCurrentPos
+			errorEast = desiredE - eastCurrentPos
+			errorDown = self.downDesired - downCurrentPos
+
+			# Time elapsed
+			dt = time.time() - prevTime
+			prevTime = time.time()
+
+			# Run some control
+			pitchControl, self.northI = self.PID(errorNorth, self.northPreviousError, self.northI, dt)
+			rollControl, self.eastI = self.PID(errorEast, self.eastPreviousError, self.eastI, dt)
+			thrustControl = -self.constrain(errorDown * self.kThrottle, self.minValD, self.maxValD)
+
+			# Update previous errror
+			self.northPreviousError = errorNorth
+			self.eastPreviousError = errorEast
+
+			# Set the controller values
+			self.pitchAngle = -self.constrain(pitchControl, self.minValNE, self.maxValNE)
+			self.rollAngle = self.constrain(rollControl, self.minValNE, self.maxValNE)
+			self.thrust = np.interp(thrustControl, self.one2one, self.zero2one)
+
+			# Send the command, sleep, and increase counter
+			self.sendAttitudeTarget(vehicle)
+			time.sleep(self.duration)
+			counter += 1
+
+	def trajectoryGen(self, IC, T, sampleRate):
+		# Define time array and storage variables
+		tt = np.linspace(0, T, round(T/sampleRate), endpoint=True)
+		pos = []; vel = []; acc = [];
+
+		# Find coeffcients of 5th order polynomial using matrix operations
+		A = np.array([[0, 0, 0, 0, 0, 1],
+					[np.power(T,5), np.power(T,4), np.power(T,3), np.power(T,2), T, 1],
+					[0, 0, 0, 0, 1, 0],
+					[5*np.power(T,4), 4*np.power(T,3), 3*np.power(T,2), 2*T, 1, 0],
+					[0, 0, 0, 2, 0, 0],
+					[20*np.power(T,3), 12*np.power(T,2), 6*T, 2, 0, 0]])
+
+		b = np.array([IC[0], IC[1], IC[2], IC[3], IC[4], IC[5]])
+
+		x = np.linalg.solve(A, b)
+
+		# Unpack coeffcients
+		A = x[0]; B = x[1]; C = x[2]; D = x[3]; E = x[4]; F = x[5];
+
+		# Calculate the trajectory properties for each time step and store
+		for t in tt:
+			pos.append(A*np.power(t,5) + B*np.power(t,4) + C*np.power(t,3) + D*np.power(t,2) + E*t + F)
+			vel.append(5*A*np.power(t,4) + 4*B*np.power(t,3) + 3*C*np.power(t,2) + 2*D*t + E)
+			acc.append(20*A*np.power(t,3) + 12*B*np.power(t,2) + 6*C*t + 2*D)
+
+		# Return the resulting position
+		return pos
+
 class DAQ:
 	def __init__(self, serialPort, serialBaud, dataNumBytes, numSignals):
 		# Class / object / constructor setup
