f0c0e3e211b37003c536fad76e84a21131aaeb73
Mark Sherstan <mshersta@ualberta.ca>
2019-06-28 14:31:50 -0600

    Commit python reboot script.

---
+++ ultrasonicPositionControl/rebootFC.py
@@ -0,0 +1,17 @@
+from dronekit import connect, VehicleMode, LocationGlobal, LocationGlobalRelative, LocationLocal
+from pymavlink import mavutil
+import serial
+import time
+
+# Connect to the Vehicle
+connection_string = "/dev/ttyS1"
+print('Connecting to vehicle on: %s\n' % connection_string)
+vehicle = connect(connection_string, wait_ready=["attitude"], baud=57600)
+
+# Quick sleep then reboot
+time.sleep(1)
+vehicle.reboot()
+time.sleep(1)
+
+# End message
+print 'Closing...'
