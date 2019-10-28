d6583ee058222f4c4ddebbd83edd0242d7d10184
Mark Sherstan <mshersta@ualberta.ca>
2019-10-28 16:42:20 -0600

    Add some example code and servo positions for the master.

--- payloads/MASTER/src/main.cpp
+++ payloads/MASTER/src/main.cpp
@@ -1,9 +1,57 @@
 #include <Arduino.h>
+#include <Servo.h>
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
+#include <Servo.h>
+
+#define button 4
+
+
+RF24 radio(7, 8); // CE, CSN
+const byte addresses[][6] = {"00001", "00002"};
+boolean buttonState = 0;
+
+void moveTo(int microSecPulse);
+
+Servo myservo;
 
 void setup() {
   // put your setup code here, to run once:
+  Serial.begin(9600);
+  myservo.attach(9);
+  moveTo(1000);
+
+
+  pinMode(button, INPUT);
+  radio.begin();
+  radio.openWritingPipe(addresses[0]); // 00001
+  radio.openReadingPipe(1, addresses[1]); // 00002
+  radio.setPALevel(RF24_PA_MIN);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  delay(5);
+  radio.startListening();
+  if ( radio.available()) {
+    while (radio.available()) {
+      int angleV = 0;
+      radio.read(&angleV, sizeof(angleV));
+      Serial.println(angleV);
+      moveTo(900);
+    }
+    delay(5);
+    radio.stopListening();
+    buttonState = digitalRead(button);
+    radio.write(&buttonState, sizeof(buttonState));
+  }
+
+  moveTo(1400);
+}
+
+void moveTo(int microSecPulse){
+  myservo.writeMicroseconds(microSecPulse);
+  Serial.println(microSecPulse);
+  delay(10000);
 }
