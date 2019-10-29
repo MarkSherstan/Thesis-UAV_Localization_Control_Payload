cd5f1a53c074347449aad0ac073070b1e8bdf898
Mark Sherstan <mshersta@ualberta.ca>
2019-10-28 23:04:41 -0600

    Talk to multiple payloads -> networking

--- payloads/MASTER/src/main.cpp
+++ payloads/MASTER/src/main.cpp
@@ -1,57 +1,61 @@
 #include <Arduino.h>
-#include <Servo.h>
-#include <SPI.h>
-#include <nRF24L01.h>
+#include <RF24Network.h>
 #include <RF24.h>
+#include <SPI.h>
 #include <Servo.h>
 
-#define button 4
+#define button    2
+#define led       3
+#define radioCE   10
+#define radioCSN  9
 
+RF24 radio(radioCE, radioCSN);
+RF24Network network(radio);
 
-RF24 radio(7, 8); // CE, CSN
-const byte addresses[][6] = {"00001", "00002"};
-boolean buttonState = 0;
+const uint16_t masterNode = 00;
+const uint16_t node01 = 01;       // Cap
+const uint16_t node02 = 02;       // Fluid
+const uint16_t node03 = 03;       // Vibration
+const uint16_t node04 = 04;       // Camera
+const uint16_t node05 = 05;       // Future port
+const uint16_t node06 = 06;       // Future port
 
-void moveTo(int microSecPulse);
-
-Servo myservo;
 
 void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(9600);
-  myservo.attach(9);
-  moveTo(1000);
-
-
-  pinMode(button, INPUT);
+  SPI.begin();
   radio.begin();
-  radio.openWritingPipe(addresses[0]); // 00001
-  radio.openReadingPipe(1, addresses[1]); // 00002
-  radio.setPALevel(RF24_PA_MIN);
+  network.begin(90, masterNode);
+  radio.setDataRate(RF24_2MBPS);
+
+  pinMode(button, INPUT_PULLUP);
+  pinMode(led, OUTPUT);
 }
+
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  delay(5);
-  radio.startListening();
-  if ( radio.available()) {
-    while (radio.available()) {
-      int angleV = 0;
-      radio.read(&angleV, sizeof(angleV));
-      Serial.println(angleV);
-      moveTo(900);
-    }
-    delay(5);
-    radio.stopListening();
-    buttonState = digitalRead(button);
-    radio.write(&buttonState, sizeof(buttonState));
+  // Update that network
+  network.update();
+
+  // Receiving
+  while (network.available()){
+    RF24NetworkHeader header;
+    unsigned long incomingData;
+    network.read(header, &incomingData, sizeof(incomingData));
+    Serial.println(incomingData);
   }
 
-  moveTo(1400);
-}
+  // Transmitting
+  unsigned long buttonState = digitalRead(button);
 
-void moveTo(int microSecPulse){
-  myservo.writeMicroseconds(microSecPulse);
-  Serial.println(microSecPulse);
-  delay(10000);
+  RF24NetworkHeader header01(node01);
+  bool ok1 = network.write(header01, &buttonState, sizeof(buttonState));
+
+  RF24NetworkHeader header02(node02);
+  bool ok2 = network.write(header02, &buttonState, sizeof(buttonState));
+
+  RF24NetworkHeader header03(node03);
+  bool ok3 = network.write(header03, &buttonState, sizeof(buttonState));
+
+  RF24NetworkHeader header04(node04);
+  bool ok4 = network.write(header04, &buttonState, sizeof(buttonState));
 }
