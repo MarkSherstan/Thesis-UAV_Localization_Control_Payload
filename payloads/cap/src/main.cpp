dd68362d73ef874e088a71d327b1d6fb107ecbc7
Mark Sherstan <mshersta@ualberta.ca>
2019-10-31 18:47:22 -0600

    Add clamp to the network and updated dev platform.

--- payloads/cap/src/main.cpp
+++ payloads/cap/src/main.cpp
@@ -1,8 +1,8 @@
 #include <Arduino.h>
 #include <Servo.h>
-#include <SPI.h>
-#include <nRF24L01.h>
+#include <RF24Network.h>
 #include <RF24.h>
+#include <SPI.h>
 #include "capPayload.h"
 
 // Pinout Analog
@@ -18,23 +18,32 @@
 #define gLED            9
 #define rLED            10
 
+// Radio and timer
+#define channel           90
+#define masterNode        00
+#define thisNode          01
+#define loopTimeMicroSec  10000
+
+// Serial port flag
+bool serialFlag = true;
+
 // Variables
-long loopTimeMicroSec = 10000;
 float force, current;
 bool switchStateA, switchStateB;
-
-// Functions
-void radioSetup(const byte address[6]);
+unsigned long dataOut;
 
 // Setup classes
 CapPayload CP;
 Servo clamp;
 RF24 radio(radioCE, radioCSN);
+RF24Network network(radio);
 
 // Run once
 void setup(){
-  // Start serial port
-  Serial.begin(115200);
+  if (serialFlag == true){
+    // Start serial port
+    Serial.begin(115200);
+  }
 
   // Configure digital pins
   CP.setUpDigitalPins(limitSwitchA, limitSwitchB, gLED, rLED);
@@ -43,8 +52,11 @@ void setup(){
   clamp.attach(clampServo);
   clamp.writeMicroseconds(1520);
 
-  // Set up radio on specified address
-  radioSetup("00001");
+  // Set up radio
+  SPI.begin();
+  radio.begin();
+  network.begin(channel, thisNode);
+  radio.setDataRate(RF24_2MBPS);
 
   // Start time sync (10000->100Hz, 5000->200Hz)
   CP.startTimeSync(loopTimeMicroSec);
@@ -52,7 +64,17 @@ void setup(){
 
 // Run forever
 void loop(){
-  // Acquire new data
+  // Update the network
+  network.update();
+
+  // Receiving data
+  while (network.available()){
+    RF24NetworkHeader header;
+    unsigned long dataIn;
+    network.read(header, &dataIn, sizeof(dataIn));
+  }
+
+  // Acquire new data from onboard sensors
   force = CP.readFSR(forceAnalog);
   current = CP.readCurrent(currentAnalog);
   switchStateA = CP.readSwitch(limitSwitchA);
@@ -69,17 +91,15 @@ void loop(){
     CP.LED_OFF(gLED);
   }
 
-  // Print data
-  CP.printData(force, current, switchStateA, switchStateB);
+  // Print data based on flag state
+  if (serialFlag == true){
+    CP.printData(force, current, switchStateA, switchStateB);
+  }
+
+  // Transmit data back
+  RF24NetworkHeader header(masterNode);
+  bool ok = network.write(header, &dataOut, sizeof(dataOut));
 
   // Stabilize sampling rate
   CP.timeSync();
-}
-
-// Radio Setup
-void radioSetup(const byte address[6]){
-  radio.begin();
-  radio.openWritingPipe(address);
-  radio.setPALevel(RF24_PA_MIN);
-  radio.stopListening();
 }
