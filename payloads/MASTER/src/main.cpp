0418de4f4da29509fc69c649dbfa3ff8fbc833a0
Mark Sherstan <mshersta@ualberta.ca>
2019-10-31 18:52:31 -0600

    Add receiver and verified coms with one payload.

--- payloads/MASTER/src/main.cpp
+++ payloads/MASTER/src/main.cpp
@@ -6,8 +6,13 @@
 
 #define button    2
 #define led       3
-#define radioCSN  9
-#define radioCE   10
+#define radioCSN  8
+#define radioCE   7
+
+// byte lastChannel;
+// int receiverInputChannel;
+// unsigned long timer;
+unsigned long analog;
 
 RF24 radio(radioCE, radioCSN);
 RF24Network network(radio);
@@ -21,13 +26,16 @@ const uint16_t node05 = 05;       // Future port
 
 
 void setup() {
+  Serial.begin(9600);
+
   SPI.begin();
   radio.begin();
   network.begin(90, masterNode);
   radio.setDataRate(RF24_2MBPS);
 
-  pinMode(button, INPUT_PULLUP);
-  pinMode(led, OUTPUT);
+  // // Set Atmega for interupt (condifugred for D8)
+  // PCICR |= (1 << PCIE0);
+  // PCMSK0 |= (1 << PCINT0);
 }
 
 
@@ -41,21 +49,39 @@ void loop() {
     unsigned long incomingData;
     network.read(header, &incomingData, sizeof(incomingData));
     digitalWrite(led, !incomingData);
-    // Serial.println(incomingData);
+    Serial.println(incomingData);
   }
 
   // Transmitting
-  unsigned long buttonState = digitalRead(button);
+  analog = analogRead(A0);
 
   RF24NetworkHeader header01(node01);
-  bool ok1 = network.write(header01, &buttonState, sizeof(buttonState));
+  bool ok1 = network.write(header01, &analog, sizeof(analog));
 
   RF24NetworkHeader header02(node02);
-  bool ok2 = network.write(header02, &buttonState, sizeof(buttonState));
+  bool ok2 = network.write(header02, &analog, sizeof(analog));
 
   RF24NetworkHeader header03(node03);
-  bool ok3 = network.write(header03, &buttonState, sizeof(buttonState));
+  bool ok3 = network.write(header03, &analog, sizeof(analog));
 
   RF24NetworkHeader header04(node04);
-  bool ok4 = network.write(header04, &buttonState, sizeof(buttonState));
+  bool ok4 = network.write(header04, &analog, sizeof(analog));
+
+
+  delay(500);
 }
+
+
+// // Inturpt function for receiver
+// ISR(PCINT0_vect){
+//   // Input changed from 0 to 1
+//   if(lastChannel == 0 && PINB & B00000001){
+//     lastChannel = 1;
+//     timer = micros();
+//   }
+//   // Input changed from 1 to 0
+//   else if(lastChannel == 1 && !(PINB & B00000001)){
+//     lastChannel = 0;
+//     receiverInputChannel = micros() - timer;
+//   }
+// }
