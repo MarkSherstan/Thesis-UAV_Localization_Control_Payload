7005300e5c583790b4744b7849038d1e8a34682a
Mark Sherstan <mshersta@ualberta.ca>
2019-11-20 13:12:20 -0700

    Set up actual python program for interacting with the payloads.

--- payloads/main.py
+++ payloads/main.py
@@ -4,19 +4,18 @@ import struct
 import serial
 
 class COMS:
-    def __init__(self, serialPort, serialBaud, numBytes):
+    def __init__(self, serialPort, serialBaud):
         # Class / object / constructor setup
         self.port = serialPort
         self.baud = serialBaud
-        self.numBytes = numBytes
         self.isReceiving = False
         self.isRun = True
         self.thread = None
-        self.dataOut = []
+        self.byteOut = 0
 
         # Connect to serial port
         try:
-            self.serialConnection = serial.Serial(serialPort, serialBaud, timeout=4)
+            self.serialConnection = serial.Serial(serialPort, serialBaud, timeout=0)
             print('Connected to ' + str(serialPort) + ' at ' + str(serialBaud) + ' BAUD.')
         except:
             print('Failed to connect with ' + str(serialPort) + ' at ' + str(serialBaud) + ' BAUD.')
@@ -42,20 +41,17 @@ class COMS:
             self.isReceiving = True
 
     def getSerialData(self):
-        # Initialize data out
-        tempData = []
+        try:
+            temp = self.serialConnection.read()
+            self.byteOut = struct.unpack('B', temp)[0]
+        except:
+            pass
 
-        # Check for header bytes and then read bytearray if header satisfied
-        if (struct.unpack('B', self.serialConnection.read())[0] is 0x9F) and
-                (struct.unpack('B', self.serialConnection.read())[0] is 0x6E):
-
-            rawData = self.serialConnection.read(self.numBytes)
-
-        # Check if data is usable otherwise repeat (recursive function)
-        if tempData:
-            self.dataOut = rawData
-        else:
-            return self.getSerialData()
+    def writeSerialData(self, msg):
+        try:
+            self.serialConnection.write(bytes([msg]))
+        except:
+            print('fail')
 
     def close(self):
         # Close the serial port connection
@@ -64,20 +60,56 @@ class COMS:
         self.serialConnection.close()
         print('Disconnected from ' + str(self.port))
 
+class QuickConnect:
+    def __init__(self):
+        # Payload protocal
+        self.ENGAGE   = 0xFE
+        self.RELEASE  = 0xFF
+
+        # Payload states
+        self.FLOATING = 0x0E
+        self.READY    = 0x5A
+        self.WAIT     = 0x5F
+        self.EXIT     = 0xEE
+
+    def engage(self, s):
+        # Send the command to engage
+        s.writeSerialData(self.ENGAGE)
+
+        # Wait till ready command is received
+        while(s.byteOut != self.READY):
+            pass
+
+        # Acknowledge ready
+        s.writeSerialData(self.READY)
+
+    def release(self, s):
+        # Send the command to engage
+        s.writeSerialData(self.RELEASE)
+
 def main():
     # Connect to serial port
-    portName = '/dev/cu.usbmodem14101'
+    portName = '/dev/cu.wchusbserial1410'
     baudRate = 9600
-    numBytes = 5
 
     # Set up the class and start the serial port
-    s = COMS(portName, baudRate, numBytes)
+    s = COMS(portName, baudRate)
     s.readSerialStart()
 
-    # Print data for 15 seconds
-    startTime = time.time()
-    while (time.time() < startTime + 15):
-        print(s.dataOut)
+    # Classes
+    q = QuickConnect()
+
+    # Run till quit
+    while(True):
+        state = input('What would you like to do? ')
+
+        # Switch case options
+        if state is 'q':
+            break
+        elif state is 'e':
+            q.engage(s)
+        elif state is 'r':
+            q.release(s)
 
     # Close the serial port
     s.close()
