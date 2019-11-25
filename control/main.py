f69dcb297c757ae173834786e2d5d1f56070c944
Mark Sherstan <mshersta@ualberta.ca>
2019-11-25 13:56:07 -0700

    Added a thread to to the frame processing for increased performance.

--- control/main.py
+++ control/main.py
@@ -1,25 +1,46 @@
 from vision import *
 
 # import threading
-# import queue
-# import time
-# import numpy as np
+
+# # import threading
+# # import queue
+# # import time
+# # import numpy as np
+#
+#
+#
+# def worker(num):
+#     """thread worker function"""
+#     print 'Worker: %s' % num
+#     return
+#
+#
+#     t = threading.Thread(target=worker, args=(i,))
+#     t.start()
+
+
 
 def main():
-    # Start capture frame class
+	# Start capture frame class and get a frame
 	CF = CaptureFrame()
 	CF.acquireFrameStart()
 
+	while (CF.frame is None):
+		time.sleep(0.1)
+
 	# Start processing frame class
-	PF = ProcessFrame()
+	PF = ProcessFrame(CF.frame)
+	PF.processFrameStart()
 
 	# Run for T secounds
 	T = int(input('Input time to run: '))
 
 	tt = time.time()
 	while time.time() < tt + T:
-		PF.getPose(CF.frame)
+		PF.frame = CF.frame
 
+	# Close the threads and other connections
+	PF.close()
 	CF.close()
 
 
--- control/vision.py
+++ control/vision.py
@@ -37,7 +37,7 @@ class CaptureFrame:
 			self.isReceiving = True
 
 	def close(self):
-		# Close the serial port connection
+		# Close the thread and camera connection
 		self.isRun = False
 		self.thread.join()
 		print('Camera thread closed')
@@ -45,9 +45,15 @@ class CaptureFrame:
 		print('Camera closed')
 
 class ProcessFrame:
-	def __init__(self, arucoDict=aruco.DICT_5X5_1000):
+	def __init__(self, firstFrame):
+		# Threading parameters
+		self.isReceiving = False
+		self.isRun = True
+		self.thread = None
+		self.frame = firstFrame
+
 		# Aruco dictionary to be used
-		self.arucoDict = aruco.Dictionary_get(arucoDict)
+		self.arucoDict = aruco.Dictionary_get(aruco.DICT_5X5_1000)
 
 		# Processing parameters
 		self.parm = aruco.DetectorParameters_create()
@@ -57,9 +63,26 @@ class ProcessFrame:
 		self.mtx = np.array([[1.01477877e+03, 0.00000000e+00, 6.59914048e+02], [0.00000000e+00, 1.01195343e+03, 3.81079689e+02], [0.00000000e+00, 0.00000000e+00, 1.00000000e+00]])
 		self.dist = np.array([[ 1.51665143e-01, 9.43194746e-01, 1.00876376e-02, 4.75462514e-03, -1.07961076e+01]])
 
-	def getPose(self, frame):
+	def processFrameStart(self):
+		# Create a thread
+		if self.thread == None:
+			self.thread = Thread(target=self.processFrame)
+			self.thread.start()
+			print('Frame processing thread start')
+
+			# Block till we start receiving values
+			while (self.isReceiving != True): #(self.frame is None) and
+				time.sleep(0.1)
+
+	def processFrame(self):
+		# Process data until closed
+		while(self.isRun):
+			self.getPose()
+			self.isReceiving = True
+
+	def getPose(self):
 		# Get frame and convert to gray
-		gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+		gray = cv2.cvtColor(self.frame, cv2.COLOR_BGR2GRAY)
 
 		# lists of ids and corners belonging to each id
 		corners, ids, _ = aruco.detectMarkers(gray, self.arucoDict, parameters=self.parm)
@@ -77,3 +100,9 @@ class ProcessFrame:
 				y = round(tvec[ii][0][1]*100,2)
 				z = round(tvec[ii][0][2]*100,2)
 				print(x,y,z)
+
+	def close(self):
+		# Close the processing thread
+		self.isRun = False
+		self.thread.join()
+		print('Frame processing thread closed')
