f41aeeb8c128911f59cb0b19151048f02772379b
Mark Sherstan <mshersta@ualberta.ca>
2019-11-25 11:03:07 -0700

    Process frame class working

--- control/vision.py
+++ control/vision.py
@@ -46,91 +46,63 @@ class CaptureFrame:
 		self.cam.release()
 		print('Camera closed')
 
-class ImgProcess:
-	def __init__(self, arucoDict, captureClass):
+class ProcessFrame:
+	def __init__(self, arucoDict=aruco.DICT_5X5_1000):
+		# Aruco dictionary to be used
 		self.arucoDict = aruco.Dictionary_get(arucoDict)
 
-		self.captureClass = captureClass
+		# Processing parameters
+		self.parm = aruco.DetectorParameters_create()
+		self.parm.adaptiveThreshConstant = 10
 
+		# Calibration values
 		self.mtx = np.array([[1.01477877e+03, 0.00000000e+00, 6.59914048e+02], [0.00000000e+00, 1.01195343e+03, 3.81079689e+02], [0.00000000e+00, 0.00000000e+00, 1.00000000e+00]])
 		self.dist = np.array([[ 1.51665143e-01, 9.43194746e-01, 1.00876376e-02, 4.75462514e-03, -1.07961076e+01]])
 
-	def getCalibration(self):
-		# Open file, retrieve variables, and close
-		file = open('calibration.pckl', 'rb')
-		self.mtx, self.dist = pickle.load(file)
-		file.close()
+	def getPose(self, frame):
+		# Get frame and convert to gray
+		gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-	def getPose(self):
-		# Get calibration data
-		try:
-			self.getCalibration()
-		except:
-			print('Calibration not found!')
+		# lists of ids and corners belonging to each id
+		corners, ids, _ = aruco.detectMarkers(gray, self.arucoDict, parameters=self.parm)
 
-		# Set parameters
-		parameters = aruco.DetectorParameters_create()
-		parameters.adaptiveThreshConstant = 10
+		# Only continue if a marker was found
+		if np.all(ids != None):
+			# Estimate the pose
+			rvec, tvec, _ = aruco.estimatePoseSingleMarkers(corners, 0.05, self.mtx, self.dist)
 
-		while(True):
-			# Get frame and convert to gray
-			gray = cv2.cvtColor(self.captureClass.frame, cv2.COLOR_BGR2GRAY)
-
-			# lists of ids and corners belonging to each id
-			corners, ids, _ = aruco.detectMarkers(gray, self.arucoDict, parameters=parameters)
-
-			# Only continue if a marker was found
-			if np.all(ids != None):
-				# Estimate the pose
-				rvec, tvec, _ = aruco.estimatePoseSingleMarkers(corners, 0.05, self.mtx, self.dist)
-
-				# Draw axis for each aruco marker found
-				for ii in range(0, ids.size):
-					aruco.drawAxis(frame, self.mtx, self.dist, rvec[ii], tvec[ii], 0.1)
-
-				# Draw square around markers
-				aruco.drawDetectedMarkers(frame, corners)
-
-				# Print ids found in top left
-				idz = ''
-				for ii in range(0, ids.size):
-					idz += str(ids[ii][0])+' '
-					x = round(tvec[ii][0][0]*100,2)
-					y = round(tvec[ii][0][1]*100,2)
-					z = round(tvec[ii][0][2]*100,2)
-					print(x,y,z)
-
-			# display the resulting frame
-			cv2.imshow('frame', self.captureClass.frame)
-			if cv2.waitKey(1) & 0xFF == ord('q'):
-				break
-
-		# When complete close everything down
-		cam.release()
-		cv2.destroyAllWindows()
+			# Print ids found in top left
+			idz = ''
+			for ii in range(0, ids.size):
+				idz += str(ids[ii][0])+' '
+				x = round(tvec[ii][0][0]*100,2)
+				y = round(tvec[ii][0][1]*100,2)
+				z = round(tvec[ii][0][2]*100,2)
+				print(x,y,z)
 
 def main():
-	IC = CaptureFrame()
-	IC.acquireFrameStart()
+	# Start capture frame class
+	CF = CaptureFrame()
+	CF.acquireFrameStart()
+
+	# Start processing frame class
+	PF = ProcessFrame()
+
+	# Run for T secounds
+	T = int(input('Input time to run: '))
 
 	tt = time.time()
+	while time.time() < tt + T:
+		# frame =
 
-	while time.time() < tt + 10:
-		cv2.imshow('frame', IC.frame)
-		if cv2.waitKey(1) & 0xFF == ord('q'):
-			return
+		PF.getPose(CF.frame)
+		# cv2.imshow('frame', frame)
+		# if cv2.waitKey(1) & 0xFF == ord('q'):
+		# 	return
 
-	IC.close()
-
-	# time.sleep(3)
-	# IP = ImgProcess(aruco.DICT_5X5_1000, IC)
-	#
-	#
-	# IP.getPose()
-
-	# ac.getCalibration()
-	# ac.trackAruco()
-	# ac.blockManipulator(rec=True)
+	# Clean up
+	# cv2.destroyAllWindows()
+	CF.close()
 
 # Main loop
 if __name__ == '__main__':
