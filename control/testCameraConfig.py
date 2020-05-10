8e97d152c9ed5e8d52f786d2bb30e711c90c75a9
Mark Sherstan <mshersta@ualberta.ca>
2020-05-09 23:08:57 -0600

    Automate and simplify run. Print useful results

--- control/testCameraConfig.py
+++ control/testCameraConfig.py
@@ -1,28 +1,32 @@
 import cv2
 import time
 
+# Set desired parameters
+desiredWidth  = 1920
+desiredHeight = 1080
+desiredFPS  = 30
+
 # Camera properties 
 cam = cv2.VideoCapture(0)
-cam.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)
-cam.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
-cam.set(cv2.CAP_PROP_FPS, 10)
+cam.set(cv2.CAP_PROP_FRAME_WIDTH, desiredWidth)
+cam.set(cv2.CAP_PROP_FRAME_HEIGHT, desiredHeight)
+cam.set(cv2.CAP_PROP_FPS, desiredFPS)
 
 # Counting variables
 frameCount = 0
 startTime = time.time()
 
-while (True):
-    try:
-        _, frame = cam.read()
-        frameCount += 1
-    except KeyboardInterrupt:
-        break
+# Run test
+print('Test running for 10 secounds')
+while (time.time() < startTime+10):
+    _, frame = cam.read()
+    frameCount += 1
 
+# Record final time and frame size
 endTime = time.time()
+actualHeight, actualWidth, _ = frame.shape 
 
-
-height, width, _ = frame.shape 
-print('Width: ', width, ' Height: ', height)
-
-print('Frames: ', frameCount, 'Time: ', (endTime - startTime))
-print('FPS: ', frameCount / (endTime - startTime))
+# Print results
+print('Frame Width\tD: ', desiredWidth, '\tA: ', actualWidth)
+print('Frame Height\tD: ', desiredHeight, '\tA: ', actualHeight)
+print('FPS\t\tD: ', desiredFPS, '\t\tA: ', round(frameCount / (endTime - startTime),2))
