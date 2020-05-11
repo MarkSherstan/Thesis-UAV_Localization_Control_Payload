d4c8e80286a0130a2df486ab7359dc4c7e10d9b1
Mark Sherstan <mshersta@ualberta.ca>
2020-05-10 22:32:17 -0600

    Clean up printing  and remove imshow

--- control/testArucoThreading.py
+++ control/testArucoThreading.py
@@ -7,7 +7,6 @@ def main():
     desiredHeight = 480     # 1080, 720,  600, 480
     desiredFPS    = 30
     autoFocus     = False
-    showFrame     = False
 
     # Camera properties 
     v = Vision(desiredWidth, desiredHeight, desiredFPS, autoFocus)
@@ -21,11 +20,12 @@ def main():
     # Run test
     print('Test running for 10 secounds\n')
     while (time.time() < startTime+10):
+        # Print pose info
+        print('N: %0.2f E: %0.2f D: %0.2f Y: %0.2f' % (v.North, v.East, v.Down, v.Yaw))
+        time.sleep(0.2)
+        
+        # Increment timer 
         loopCount += 1
-        if showFrame is True:
-            cv2.imshow('Frame', v.frame)
-            if cv2.waitKey(1) & 0xFF == ord('q'):
-                break
 
     # Record final time and frame size            
     endTime = time.time()
@@ -38,9 +38,8 @@ def main():
     print('Pose rate\tD: ', desiredFPS, '\t\tA: ', round(v.poseCount / (endTime - startTime),2))
     print('Main process\tD: ', desiredFPS, '\t\tA: ', round(loopCount / (endTime - startTime),2))
 
-    # Close any windows if there are any
+    # Close threads and camera connection
     v.close()
-    cv2.destroyAllWindows() 
     
 # Main loop
 if __name__ == '__main__':
