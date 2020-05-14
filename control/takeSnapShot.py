902be0bc150d39923be33000aaa4a50335bb3baf
MarkSherstan <mshersta@ualberta.ca>
2020-05-14 14:21:34 -0600

    Update to 1280x960 to avoid confusion

--- control/takeSnapShot.py
+++ control/takeSnapShot.py
@@ -2,9 +2,9 @@ import cv2
 import time
 
 cam = cv2.VideoCapture(0)
-cam.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
-cam.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
-cam.set(cv2.CAP_PROP_FPS, 60)
+cam.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
+cam.set(cv2.CAP_PROP_FRAME_HEIGHT, 960)
+cam.set(cv2.CAP_PROP_FPS, 30)
 time.sleep(0.2)
 
 _, frame = cam.read()
