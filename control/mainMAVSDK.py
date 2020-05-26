9f4e2ae943c6a2f4fa3c83aa58421c1572c2aedc
Mark Sherstan <mshersta@ualberta.ca>
2020-05-26 00:25:05 -0600

    Print logging working as expected

--- control/mainMAVSDK.py
+++ control/mainMAVSDK.py
@@ -1,17 +1,18 @@
 import asyncio
+import time 
 
 from mavsdk import System
 from mavsdk import (Attitude, OffboardError, Telemetry)
 
+# Client code must specify a setpoint before starting offboard mode.
+# Mavsdk automatically resends setpoints at 20Hz (PX4 Offboard mode requires that setpoints are minimally resent at 2Hz). If more precise control is required, clients can call the setpoint methods at whatever rate is required.
 
 async def print_attitude(drone):
     async for bodyAttitude in drone.telemetry.attitude_euler():
-        print(bodyAttitude.roll_deg)
-        
+        print(time.time(), ",", bodyAttitude.roll_deg, ",", bodyAttitude.pitch_deg, ",", bodyAttitude.yaw_deg)
 
 async def run():
     """ Does Offboard control using attitude commands. """
-
     drone = System()
     await drone.connect(system_address="serial:///dev/cu.usbmodem14101:921600") #serial://[Dev_Node][:Baudrate]
 
@@ -37,16 +38,21 @@ async def run():
         await drone.action.disarm()
         return
 
+
+    # Set telemtry to 20 Hz 
+    # await drone.telemetry.set_rate_attitude(20)
+    # https://github.com/mavlink/MAVSDK-Python/blob/9ac7113a88f49281c698997c55e9d46f8a8fd02c/mavsdk/generated/telemetry.py#L3553
+    # await drone.telemetry.set_rate_attitude(20)
+    # await asyncio.sleep(2)
+    
+    
+    await asyncio.sleep(2)
+    asyncio.ensure_future(print_attitude(drone))
+
     print("-- Roll 30 at 60% thrust")
     await drone.offboard.set_attitude(Attitude(30.0, 0.0, 0.0, 0.6))
     await asyncio.sleep(2)
-    
-    
-    for ii in range(10):
-        asyncio.ensure_future(print_attitude(drone))
-        await asyncio.sleep(0.2)
-  
-
+        
     print("-- Roll -30 at 60% thrust")
     await drone.offboard.set_attitude(Attitude(-30.0, 0.0, 0.0, 0.6))
     await asyncio.sleep(2)
