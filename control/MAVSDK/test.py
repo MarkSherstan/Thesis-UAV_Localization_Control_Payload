e1198ee99d96dba008ef3d54f280ebfa24f36ccb
MarkSherstan <mshersta@ualberta.ca>
2020-05-27 04:33:26 +0000

    Update test script to validate actual servo output

--- control/MAVSDK/test.py
+++ control/MAVSDK/test.py
@@ -1,48 +1,64 @@
 import asyncio
 import time 
 
+from mavsdk import System
+from mavsdk import (Attitude, AttitudeRate, OffboardError, Telemetry)
+
+from controller import Controller
+
+
 # Global storage variable
 storage = []
 
-# Some function - Controller or getting data
-async def print_attitude(var):
-    out = 1*var
-    return out
+async def getAttitude(drone):
+    async for bodyAttitude in drone.telemetry.attitude_euler():
+        return bodyAttitude.roll_deg, bodyAttitude.pitch_deg, bodyAttitude.yaw_deg
 
-# Main loop 
 async def run():
-    # Initialize values
-    global storage
-    ii = 0 
-    timer = time.time()
+    # Mac OS
+    # drone = System()
+    # await drone.connect(system_address="serial:///dev/cu.usbmodem14101:921600")
 
-    # Run forever till broken 
-    while(True):
-        # Small pause - 10 Hz
-        await asyncio.sleep(0.1)
-        
-        # Perform the calculation
-        A = await asyncio.ensure_future(print_attitude(ii))
-        storage.append(A)
-        
-        # Print data
-        print(1/(time.time()-timer), A)
-        
-        # Reset and icrement timer
-        timer = time.time()
-        ii += 1 
+    # Linux
+    drone = System(mavsdk_server_address='localhost', port=50051)
+    await drone.connect()
+
+    print("Waiting for drone to connect...")
+    async for state in drone.core.connection_state():
+        if state.is_connected:
+            print(f"Drone discovered with UUID: {state.uuid}")
+            break
+
+    # Set initial set point for attitiude (required)
+    await drone.offboard.set_attitude(Attitude(0.0, 0.0, 0.0, 0.0))
   
+    # Connect to control scheme 
+    C = Controller(250, 0, 0)
+    C.startTime = time.time()
+    
+    # Start timer 
+    timer = time.time()
+    global storage
+    
+    while(True):
+        # Send attitude command
+        await drone.offboard.set_attitude(Attitude(0.0, 0.0, 0.0, 0.5))
+        await drone.offboard.set_attitude_rate(AttitudeRate(0.0, 0.0, -45, 0.0))
         
+        # print(drone.)
+
+        time.sleep(1)
+
+        await drone.offboard.set_attitude(Attitude(0.0, 0.0, 0.0, 0.5))
+        await drone.offboard.set_attitude_rate(AttitudeRate(0.0, 0.0, 45, 0.0))
+
+        time.sleep(1)
+
 if __name__ == "__main__":
     loop = asyncio.get_event_loop()
-        
+
     try:
         loop.run_until_complete(run())
     except KeyboardInterrupt:
-        print("Caught keyboard interrupt.")    
-        print(storage)    
-    finally:
-        loop.close()
-        
-        
-
+        print("The end")    
+        # print(storage)    
