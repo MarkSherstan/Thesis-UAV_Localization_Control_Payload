229d134d0ceef24498965b9212f1ae7409e64e70
Mark Sherstan <mshersta@ualberta.ca>
2020-05-26 21:43:57 -0600

    Clean up class and input output variables

--- control/MAVSDK/controller.py
+++ control/MAVSDK/controller.py
@@ -1,5 +1,3 @@
-from pymavlink import mavutil
-from threading import Thread
 import numpy as np
 import time
 import math
@@ -11,25 +9,13 @@ class Controller:
 		self.eastDesired = eastDesired
 		self.downDesired = downDesired
 
-		# Real time data from vision class
-		self.North = 0
-		self.East  = 0
-		self.Down  = 0
-		self.Yaw   = 0
-
-		# Initial conditions
-		self.rollAngle = 0.0
-		self.pitchAngle = 0.0
-		self.yawAngle = 0.0
-		self.thrust = 0.5
-
 		# Constraints for roll, pitch, yaw and thrust
-		self.minValNE = -3.1415/12		# - 15 Degrees
-		self.maxValNE = 3.1415/12	    # + 15 Degrees
-		self.minValD = -1				# 
-		self.maxValD = 1				#
-		self.minValYaw = -3.1415/4		#
-		self.maxValYaw = 3.1415/4		#
+		self.minValNE = -3.1415/12		# -15 Deg
+		self.maxValNE = 3.1415/12	    # +15 Deg
+		self.minValD = -1				# m
+		self.maxValD = 1				# m
+		self.minValYaw = -3.1415/4		# -45 Deg/s
+		self.maxValYaw = 3.1415/4		# +45 Deg/s
 
 		# Controller PID Gains
 		self.kp = 0.3
@@ -50,8 +36,8 @@ class Controller:
 
 		# Yaw control
 		self.kYaw = 2
-		self.yawConstrained = [-3.1415/4, -3.1415/90, 3.1415/90, 3.1415/4]
-		self.yawRateInterp  = [-3.1415/2, 0, 0, 3.1415/2]
+		self.yawConstrained = [-3.1415/4, -3.1415/90, 3.1415/90, 3.1415/4] 	# [-45, -2, 2, 45] deg/s
+		self.yawRateInterp  = [-3.1415/2, 0, 0, 3.1415/2]					# [-90,  0, 0, 90] deg/s	
 
 	def constrain(self, val, minVal, maxVal):
 		return max(min(maxVal, val), minVal)
@@ -63,11 +49,11 @@ class Controller:
 		D = self.kd * ((error - errorPrev) / dt)
 		return (P + I + D), I
 
-	def positionControl(self):
+	def positionControl(self, northActual, eastActual, downActual, yawActual):
 		# Error calculations and conversion to meters
-		errorNorth = (self.northDesired - self.North) * 0.01
-		errorEast = (self.eastDesired - self.East) * 0.01
-		errorDown = (self.downDesired - self.Down) * 0.01
+		errorNorth = (self.northDesired - northActual) * 0.01
+		errorEast = (self.eastDesired - eastActual) * 0.01
+		errorDown = (self.downDesired - downActual) * 0.01
 
 		# Get time delta
 		dt = time.time() - self.startTime
@@ -75,7 +61,7 @@ class Controller:
 		# Run some control
 		rollControl, self.eastI = self.PID(errorEast, self.eastPreviousError, self.eastI, dt)
 		pitchControl, self.northI = self.PID(errorNorth, self.northPreviousError, self.northI, dt)
-		yawControl = self.constrain(self.Yaw * self.kYaw, self.minValYaw, self.maxValYaw)
+		yawControl = self.constrain(yawActual * self.kYaw, self.minValYaw, self.maxValYaw)
 		thrustControl = -self.constrain(errorDown * self.kThrottle, self.minValD, self.maxValD)
 
 		# Update previous error
@@ -83,7 +69,10 @@ class Controller:
 		self.eastPreviousError = errorEast
 
 		# Set the controller values
-		self.rollAngle = self.constrain(rollControl, self.minValNE, self.maxValNE)
-		self.pitchAngle = self.constrain(pitchControl, self.minValNE, self.maxValNE)
-		self.yawRate = np.interp(yawControl, self.yawConstrained, self.yawRateInterp)
-		self.thrust = np.interp(thrustControl, self.one2one, self.zero2one)
+		rollAngle = self.constrain(rollControl, self.minValNE, self.maxValNE)
+		pitchAngle = self.constrain(pitchControl, self.minValNE, self.maxValNE)
+		yawRate = np.interp(yawControl, self.yawConstrained, self.yawRateInterp)
+		thrust = np.interp(thrustControl, self.one2one, self.zero2one)
+
+		# Return the value
+		return rollAngle, pitchAngle, yawRate, thrust
