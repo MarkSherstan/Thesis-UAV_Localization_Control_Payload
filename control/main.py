d3afd4ea71f9d99b3de0c18dce4a576eae89683a
Mark Sherstan <mshersta@ualberta.ca>
2020-06-05 14:33:33 -0600

    Update main back to dronekit

--- control/main.py
+++ control/main.py
@@ -1,125 +1,94 @@
+from dronekit import connect, VehicleMode, LocationGlobal, LocationGlobalRelative, LocationLocal
+from multiprocessing import Process, Queue
+from controller import Controller
+from pymavlink import mavutil
+from vision import Vision
 import pandas as pd
 import statistics
 import datetime
-import asyncio
+import math
 import time 
 
-from mavsdk import System
-from mavsdk import (Attitude, AttitudeRate, OffboardError, Telemetry)
-
-from multiprocessing import Process, Queue
-from controller import Controller
-from vision import Vision
-
-# Global data variable
-data = []
-freqList = []
-
-async def getAttitude(drone):
-    try:
-        async for bodyAttitude in drone.telemetry.attitude_euler():
-            return bodyAttitude.roll_deg, bodyAttitude.pitch_deg, bodyAttitude.yaw_deg
-    except:
-        print('ERROR: Get Attitude')
-        return -1, -1, -1
-
-async def getFlightMode(drone):
-    async for flightMode in drone.telemetry.flight_mode():
-        return flightMode
-
 def getVision(Q):
+    # Vision Data
     temp = Q.get()
     return temp[0], temp[1], temp[2], temp[3]
-    
-async def run():
-    # Connect to the drone
-    drone = System(mavsdk_server_address='localhost', port=50051)
-    await drone.connect()
 
-    print("Waiting for drone to connect...")
-    async for state in drone.core.connection_state():
-        if state.is_connected:
-            print(f"Drone discovered with UUID: {state.uuid}")
-            break
+def getVehicleAttitude(UAV):
+    # Actual vehicle attitude
+    roll  = math.degrees(UAV.attitude.roll)
+    pitch = math.degrees(UAV.attitude.pitch)
+    yaw   = math.degrees(UAV.attitude.yaw)
+    return roll, pitch, yaw
 
-    # Set message rate (only works with PX4)
-    # await drone.telemetry.set_rate_attitude(35)
-    # await asyncio.sleep(1) 
+def main():
+    # Connect to the Vehicle
+    connection_string = "/dev/ttyS1"
+    print('Connecting to vehicle on: %s\n' % connection_string)
+    vehicle = connect(connection_string, wait_ready=["attitude"], baud=1500000) #, source_system=1, source_component=1)
+
+    # Set attitude request message rate (everything else is default 4 Hz)
+    msg = vehicle.message_factory.request_data_stream_encode(
+    0, 0,
+    mavutil.mavlink.MAV_DATA_STREAM_EXTRA1,
+    100, # Rate (Hz)
+    1)  # Turn on
+    vehicle.send_mavlink(msg)    
     
-    # Connect to control scheme
-    northDesired = 500
-    eastDesired = 0
-    downDesired = 0
-    C = Controller(northDesired, eastDesired, downDesired)
- 
     # Connect to vision, create the queue, and start the core
     V = Vision()
     Q = Queue()
     P = Process(target=V.processFrame, args=(Q, ))
     P.start()
-
-    # Client code must specify a setpoint before starting offboard mode.
-    for _ in range(10):
-        await drone.offboard.set_attitude(Attitude(0.0, 0.0, 0.0, 0.0))
-
-    # Variables
-    global data
-    global freqList
-    sleepRate = 0.02
     
-    # Start timers
+    # Connect to control scheme
+    northDesired = 500
+    eastDesired = 0
+    downDesired = 0
+    C = Controller(northDesired, eastDesired, downDesired, vehicle)
+
+    # Logging variables
+    freqList = []
+    data = []
+    
+    # Loop timer
     startTime = time.time()
     loopTimer = time.time()
-    C.timer = time.time()
+    C.startTimers()
     
-    while(True):
-        # Get vision data and clear the queue
-        northV, eastV, downV, yawV = getVision(Q)
-        while not Q.empty():
-            Q.get()
-        
-        # Calculate controller outputs
-        rollControl, pitchControl, yawControl, thrustControl = C.positionControl(northV, eastV, downV, yawV)         
-
-        # Execute control
-        try:
-            await drone.offboard.set_attitude(Attitude(rollControl, pitchControl, yaw, thrustControl))
-            await asyncio.sleep(sleepRate)
-            await drone.offboard.set_attitude_rate(AttitudeRate(0.0, 0.0, yawControl, thrustControl))
-            await asyncio.sleep(sleepRate)
-        except:
-            print('ERROR: Execute Control')
-        
-        # Get current attitude
-        roll, pitch, yaw = await getAttitude(drone)
-        await asyncio.sleep(sleepRate)
-
-        # Print data
-        freqLocal = (1 / (time.time() - loopTimer))
-        freqList.append(freqLocal)
-        print('f: {:<8.0f} N: {:<8.0f} E: {:<8.0f} D: {:<8.0f} Y: {:<8.2f}'.format(freqLocal, northV, eastV, downV, yawV))
-        print('R: {:<8.2f} P: {:<8.2f} Y: {:<8.2f} r: {:<8.2f} p: {:<8.2f} y: {:<8.2f} t: {:<8.2f}'.format(roll, pitch, yaw, rollControl, pitchControl, yawControl, thrustControl))
-        loopTimer = time.time()
-        
-        # Log data
-        data.append([time.time()-startTime, freqLocal, 
-                    northV, eastV, downV, yawV, 
-                    northDesired, eastDesired, downDesired, 
-                    roll, pitch, yaw, 
-                    rollControl, pitchControl, yawControl, thrustControl])
-        
-          
-if __name__ == "__main__":
-    loop = asyncio.get_event_loop()
-
     try:
-        loop.run_until_complete(run())
+        while(True):
+            # Get vision data
+            northV, eastV, downV, yawV = getVision(Q) # Check these loop rates. Do we need to clear the q?
+            
+            # Calculate control and execture
+            rollControl, pitchControl, yawControl, thrustControl = C.positionControl(northV, eastV, downV, yawV)         
+
+            # Get actual vehicle data
+            roll, pitch, yaw = getVehicleAttitude(vehicle)
+            
+            # Print data
+            freqLocal = (1 / (time.time() - loopTimer))
+            freqList.append(freqLocal)
+            print('f: {:<8.0f} N: {:<8.0f} E: {:<8.0f} D: {:<8.0f} Y: {:<8.2f}'.format(freqLocal, northV, eastV, downV, yawV))
+            print('R: {:<8.2f} P: {:<8.2f} Y: {:<8.2f} r: {:<8.2f} p: {:<8.2f} y: {:<8.2f} t: {:<8.2f}'.format(roll, pitch, yaw, rollControl, pitchControl, yawControl, thrustControl))
+            loopTimer = time.time()
+
+            # Log data
+            data.append([time.time()-startTime, freqLocal, 
+                        northV, eastV, downV, yawV, 
+                        northDesired, eastDesired, downDesired, 
+                        roll, pitch, yaw, 
+                        rollControl, pitchControl, yawControl, thrustControl])
+        
     except KeyboardInterrupt:
-        # Print closing remarks
-        print("Closing...") 
-        time.sleep(1)
+        # Print final remarks
+        print('Closing')
     finally:
-        # Post some sampling data
+        # Get message rate
+        C.close()
+        
+        # Post main loop rate
         print("Average loop rate: ", round(statistics.mean(freqList),2), "+/-", round(statistics.stdev(freqList), 2))
 
         # Write data to a data frame
@@ -135,14 +104,5 @@ if __name__ == "__main__":
         df.to_csv(fileName, index=None, header=True)
         print('File saved to:' + fileName)
 
-
-# Usage
-#   tmux (or tmux attach)
-#   cd /home/odroid/MAVSDK/build/default/src/backend/src
-#   ./mavsdk_server -p 50051 serial:///dev/ttyS1:921600
-#   cntrl+b 
-#   d
-#
-#   workon cv
-#   cd /home/odroid/Desktop/UAV-Sampling-Control-System/control
-#   python main.py
+if __name__ == "__main__":
+    main()
