ae755873624fcf3f757883142aed4d503b42ea5a
Mark Sherstan <mshersta@ualberta.ca>
2020-06-16 22:59:37 +0000

    poking test

--- control/controller.py
+++ control/controller.py
@@ -19,14 +19,14 @@ class Controller:
         self.yawRateConstrain = [-10, 10]           # Deg / s
 
         # PID Gains: NORTH (pitch)
-        self.kp_NORTH = 0.04
+        self.kp_NORTH = 0.03
         self.ki_NORTH = 0.003
         self.kd_NORTH = 0.0001
 
         # PID Gains: EAST (roll)
-        self.kp_EAST = self.kp_NORTH*0.3
-        self.ki_EAST = self.ki_NORTH*0.3
-        self.kd_EAST = self.kd_NORTH*0.3
+        self.kp_EAST = self.kp_NORTH*0.25
+        self.ki_EAST = self.ki_NORTH*0.25
+        self.kd_EAST = self.kd_NORTH*0.25
 
         # PID Gains: DOWN (thrust)
         self.kp_DOWN = 0.0001
