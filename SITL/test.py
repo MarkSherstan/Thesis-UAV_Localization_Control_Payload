e81594263dfc78c8dd3b278b976cd54de0da7858
MarkSherstan <mshersta@ualberta.ca>
2020-06-17 14:45:12 -0600

    Need to switch to an online approach

--- SITL/test.py
+++ SITL/test.py
@@ -1,51 +1,33 @@
-import simdkalman
+# https://github.com/oseiskar/simdkalman/issues/3
 import numpy as np
-import numpy.random as random
+import simdkalman
+import pandas as pd
 import matplotlib.pyplot as plt
 
-# Configure the filter
+np.random.seed(0)
+data = np.random.normal(size=150)
+
 kf = simdkalman.KalmanFilter(
-    state_transition = np.array([[1,1],[0,1]]),     # A
-    process_noise = np.diag([0.1, 0.01]),           # Q
-    observation_model = np.array([[1,0]]),          # H
-    observation_noise = 1.0)                        # R
+    state_transition = [[1,1],[0,1]],        # A
+    process_noise = np.diag([0.05, 0.002]),  # Q
+    observation_model = np.array([[1,0]]),   # H
+    observation_noise = 20.0)                # R
 
-# simulate 100 random walk time series
-# introduce 10% of NaNs denoting missing values
-rand = lambda: random.normal(size=(1, 200))
-data = np.cumsum(np.cumsum(rand()*0.02, axis=1) + rand(), axis=1) + rand()*3
-data[random.uniform(size=data.shape) < 0.1] = np.nan
+kf_results = kf.smooth(data)
 
-# fit noise parameters to data with the EM algorithm (optional)
-kf = kf.em(data, n_iter=10)
+t = range(data.size)
 
-# smooth and explain existing data
-smoothed = kf.smooth(data)
+def kf_smooth(y):
+    return kf.smooth(y).observations.mean
 
-# predict new data
-pred = kf.predict(data, 15)
+def kf_filter(y):
+    return kf.compute(y, 0, filtered=True).filtered.observations.mean
 
-
-# Plot the data
-ax1 = plt.gca()
-
-plt.title("time series")
-x = np.arange(0, data.shape[1])
-
-ax1.plot(x, data[0], 'b.', label="data")
-
-smoothed_obs = smoothed.observations.mean[0]
-obs_stdev = np.sqrt(smoothed.observations.cov[0])
-ax1.plot(x, smoothed_obs, 'r-', label="smoothed")
-ax1.plot(x, smoothed_obs - obs_stdev, 'k--', label="67% confidence")
-ax1.plot(x, smoothed_obs + obs_stdev, 'k--')
-
-x_pred = np.arange(data.shape[1], data.shape[1]+pred.observations.mean.shape[1])
-y_pred = pred.observations.mean[0]
-pred_stdev = np.sqrt(pred.observations.cov[0])
-ax1.plot(x_pred, y_pred, 'b-', label="predicted")
-ax1.plot(x_pred, y_pred + pred_stdev, 'k--')
-ax1.plot(x_pred, y_pred - pred_stdev, 'k--')
-ax1.legend()
-
+plt.plot(t, data, 'kx', alpha=0.2, label='data')
+plt.plot(t[:100], data[:-50], 'bx', alpha=0.4, label='first 100')
+plt.plot(t, kf_smooth(data), 'k-', label='smoothed full')
+plt.plot(t, kf_filter(data), 'k--', label='filtered full')
+plt.plot(t[:100], kf_smooth(data[:100]), 'b-', label='smoothed first 100')
+plt.plot(t[:100], kf_filter(data[:100]), 'b--', label='filtered first 100')
+plt.legend()
 plt.show()
