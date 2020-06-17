2de07e75315fd5eb7e4c42a043c392c299995251
MarkSherstan <mshersta@ualberta.ca>
2020-06-17 11:39:12 -0600

    Clean up unused parts

--- SITL/test.py
+++ SITL/test.py
@@ -1,44 +1,40 @@
-"""
-Smoothing and prediction examples with figures. Needs matplotlib
-to display the graphics.
-
-Example output:
-https://github.com/oseiskar/simdkalman/blob/master/doc/example.png
-"""
-
 import simdkalman
 import numpy as np
 import numpy.random as random
+import matplotlib.pyplot as plt
 
+# Configure the filter
 kf = simdkalman.KalmanFilter(
-    state_transition = np.array([[1,1],[0,1]]),
-    process_noise = np.diag([0.1, 0.01]),
-    observation_model = np.array([[1,0]]),
-    observation_noise = 1.0)
+    state_transition = np.array([[1,1],[0,1]]),     # A
+    process_noise = np.diag([0.1, 0.01]),           # Q
+    observation_model = np.array([[1,0]]),          # H
+    observation_noise = 1.0)                        # R
 
 # simulate 100 random walk time series
+# introduce 10% of NaNs denoting missing values
 rand = lambda: random.normal(size=(100, 200))
 data = np.cumsum(np.cumsum(rand()*0.02, axis=1) + rand(), axis=1) + rand()*3
-
-# introduce 10% of NaNs denoting missing values
 data[random.uniform(size=data.shape) < 0.1] = np.nan
 
 # fit noise parameters to data with the EM algorithm (optional)
-kf = kf.em(data, n_iter=10)
+# kf = kf.em(data, n_iter=10)
 
 # smooth and explain existing data
 smoothed = kf.smooth(data)
+
+
 # predict new data
 pred = kf.predict(data, 15)
 # could be also written as
 # r = kf.compute(data, 15); smoothed = r.smoothed; pred = r.predicted
 
-import matplotlib.pyplot as plt
 
 # show the first 3 smoothed time series
 for i in range(3):
+    
+    ax1 = plt.gca()
 
-    _, (ax1, ax2) = plt.subplots(2, 1, sharex=True)
+    # _, (ax1, ax2) = plt.subplots(2, 1, sharex=True)
     plt.title("time series %d" % (i+1))
 
     x = np.arange(0, data.shape[1])
@@ -59,17 +55,17 @@ for i in range(3):
     ax1.plot(x_pred, y_pred - pred_stdev, 'k--')
     ax1.legend()
 
-    trend = smoothed.states.mean[i,:,1]
-    trend_stdev = np.sqrt(smoothed.states.cov[i,:,1,1])
-    ax2.plot(x, trend, 'g-', label="trend")
-    ax2.plot(x, trend - trend_stdev, 'k--', label="67% confidence")
-    ax2.plot(x, trend + trend_stdev, 'k--')
+    # trend = smoothed.states.mean[i,:,1]
+    # trend_stdev = np.sqrt(smoothed.states.cov[i,:,1,1])
+    # ax2.plot(x, trend, 'g-', label="trend")
+    # ax2.plot(x, trend - trend_stdev, 'k--', label="67% confidence")
+    # ax2.plot(x, trend + trend_stdev, 'k--')
 
-    trend_pred = pred.states.mean[i,:,1]
-    trend_pred_stdev = np.sqrt(pred.states.cov[i,:,1,1])
-    ax2.plot(x_pred, trend_pred, 'b-', label='predicted')
-    ax2.plot(x_pred, trend_pred + trend_pred_stdev, 'k--')
-    ax2.plot(x_pred, trend_pred - trend_pred_stdev, 'k--')
-    ax2.legend()
+    # trend_pred = pred.states.mean[i,:,1]
+    # trend_pred_stdev = np.sqrt(pred.states.cov[i,:,1,1])
+    # ax2.plot(x_pred, trend_pred, 'b-', label='predicted')
+    # ax2.plot(x_pred, trend_pred + trend_pred_stdev, 'k--')
+    # ax2.plot(x_pred, trend_pred - trend_pred_stdev, 'k--')
+    # ax2.legend()
 
     plt.show()
