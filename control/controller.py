11d959b3c3c99e8d807004a89843fd5bcd690117
Mark Sherstan <mshersta@ualberta.ca>
2020-06-19 01:59:51 +0000

    Increase integral a little

--- control/controller.py
+++ control/controller.py
@@ -20,13 +20,13 @@ class Controller:
 
         # PID Gains: NORTH (pitch)
         self.kp_NORTH = 0.02
-        self.ki_NORTH = 0.003
+        self.ki_NORTH = 0.004
         self.kd_NORTH = 0.0001
 
         # PID Gains: EAST (roll)
-        self.kp_EAST = self.kp_NORTH*0.2
-        self.ki_EAST = self.ki_NORTH*0.2
-        self.kd_EAST = self.kd_NORTH*0.2
+        self.kp_EAST = self.kp_NORTH*0.3
+        self.ki_EAST = self.ki_NORTH*0.6
+        self.kd_EAST = self.kd_NORTH
 
         # PID Gains: DOWN (thrust)
         self.kp_DOWN = 0.0001
