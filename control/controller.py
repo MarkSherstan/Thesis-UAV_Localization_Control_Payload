6078a5cec7ac157baf22fd840ab312ff32c0e054
Mark Sherstan <mshersta@ualberta.ca>
2020-06-24 16:51:19 +0000

    Zero yaw was very smooth

--- control/controller.py
+++ control/controller.py
@@ -28,7 +28,7 @@ class Controller:
         self.kd_DOWN = 0.0
 
         # PID Gains: YAW (yaw rate)
-        self.kp_YAW = 4.0
+        self.kp_YAW = 0 #4.0
         self.ki_YAW = 0.0
         self.kd_YAW = 0.0
 
