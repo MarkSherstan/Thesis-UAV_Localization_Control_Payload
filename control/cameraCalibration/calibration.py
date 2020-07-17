d6f18058d64a0bcfee90029ec3be5df8bf06200f
Mark Sherstan <mshersta@ualberta.ca>
2020-07-17 08:06:22 -0600

    Add figure generation for calibration

--- control/cameraCalibration/calibration.py
+++ control/cameraCalibration/calibration.py
@@ -192,6 +192,72 @@ class CalibrateCamera:
         pickle.dump((self.mtx, self.dist), f)
         f.close()
 
+    def generateCalibrationImg(self, rows=7, columns=5, lengthSquare=0.035, lengthMarker=0.0175):
+        # Create charuco board with actual measured dimensions from print out
+        board = aruco.CharucoBoard_create(
+                    squaresX=columns,
+                    squaresY=rows,
+                    squareLength=lengthSquare,
+                    markerLength=lengthMarker,
+                    dictionary=self.arucoDict)
+
+        # Get image paths from calibration folder
+        paths = glob.glob(self.calibrationDir + '*' + self.imgExtension)
+
+        # Image list
+        data = []
+        
+        # Counter
+        ii = 0
+
+        # Loop through all images
+        for filePath in paths:
+            # Read image and convert to gray
+            img = cv2.imread(filePath)
+            gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+            # Find aruco markers in the query image
+            corners, ids, _ = aruco.detectMarkers(image=gray, dictionary=self.arucoDict)
+
+            # Outline the aruco markers found in the query image
+            aruco.drawDetectedMarkers(image=img, corners=corners)
+
+            # Get charuco corners and ids from detected aruco markers
+            response, charucoCorners, charucoIDs = aruco.interpolateCornersCharuco(
+                    markerCorners=corners,
+                    markerIds=ids,
+                    image=gray,
+                    board=board)
+
+            # If at least 20 corners were found
+            if response > 20:
+                # Draw the Charuco board detected to show calibration results
+                aruco.drawDetectedCornersCharuco(image=img, charucoCorners=charucoCorners, charucoIds=charucoIDs)
+                
+                # Resize image and save to list
+                data.append(cv2.resize(img,(320,180)))
+                
+                # Incrememnt counter
+                ii += 1     
+            else:
+                # Error message
+                print('Error in: ' + str(filePath))
+
+            # Exit condition
+            if (ii >= 9):
+                break
+
+        # Create matrix image
+        A = np.concatenate((data[0], data[1], data[2]), axis=1)
+        B = np.concatenate((data[3], data[4], data[5]), axis=1)
+        C = np.concatenate((data[6], data[7], data[8]), axis=1)
+        D = np.concatenate((A, B, C), axis=0)
+                
+        # Display matrix image
+        cv2.imshow('Photo Matrix', D)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+    
     def getCalibration(self):
         # Open file, retrieve variables, and close
         file = open('resources/calibration.pckl', 'rb')
@@ -317,16 +383,17 @@ def main():
     # CC.generateArucoBoard()
     # CC.generateArucoMarker()
 
-    CC.startCamera(desiredWidth=1280, desiredHeight=720, desiredFPS=30, src=0)
+    # CC.startCamera(desiredWidth=1280, desiredHeight=720, desiredFPS=30, src=0)
     # CC.captureCalibrationImages()
     # CC.calibrateCamera()
+    CC.generateCalibrationImg()
 
     # CC.getCalibration()
     # print(CC.mtx)
     # print(CC.dist)
 
     # CC.trackAruco()
-    CC.trackArucoBoard()
+    # CC.trackArucoBoard()
 
 # Main loop
 if __name__ == '__main__':
