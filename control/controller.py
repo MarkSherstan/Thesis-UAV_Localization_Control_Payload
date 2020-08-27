5f1a386ab8b6e3c8853c69021f1dac843d18d510
MarkSherstan <mshersta@ualberta.ca>
2020-08-27 14:23:18 -0600

    reduce D term

--- control/controller.py
+++ control/controller.py
@@ -22,7 +22,7 @@ class Controller:
         # PID Gains: EAST (roll)
         self.kp_EAST = 0.05 #0.075
         self.ki_EAST = 0.003       # Max 1.5 deg with 500 bounds
-        self.kd_EAST = 0.1  #0.065
+        self.kd_EAST = 0.05  #0.065
 
         # PID Gains: DOWN (thrust)
         self.kp_DOWN = 0.002
