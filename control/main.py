1c95846566d6fde2661dd504e9698fa0efb41fcf
Mark Sherstan <mshersta@ualberta.ca>
2020-09-08 07:48:38 -0600

    Update profiling to be its own script

--- control/main.py
+++ control/main.py
@@ -10,7 +10,6 @@ import pandas as pd
 import numpy as np
 import statistics
 import datetime
-import yappi
 import math
 import time
 
@@ -219,19 +218,4 @@ def main():
         print('File saved to:' + fileName)
 
 if __name__ == "__main__":
-    # Get yappi to do its thing
-    yappi.set_clock_type("wall") # cpu
-    yappi.start(builtins=True)
     main()
-    yappi.stop()
- 
-    # Retrieve thread stats by their Yappi thread id 
-    threads = yappi.get_thread_stats()
-    ii = 0
-    for thread in threads:
-        print("Function stats for (%s) (%d)" % (thread.name, thread.id))
-        temp = yappi.get_func_stats(ctx_id=thread.id)
-        temp.print_all()
-        temp.save('profiling/' + thread.name + str(ii), type='pstat') # qcachegrind
-        ii += 1
-        print('\n\n')
---
+++ control/profiling/profile.sh
@@ -0,0 +1,4 @@
+#!/bin/bash  
+# Run with `bash profile.sh`
+
+sudo /home/mark/.virtualenvs/py3cv4/bin/python3.6 profile.py
