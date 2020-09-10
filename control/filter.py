db331ca78d2988c85cfcf420bd3438ce630f4791
Mark Sherstan <mshersta@ualberta.ca>
2020-09-09 20:58:09 -0600

    Initial layout of NxN kalman filter solve

--- control/filter.py
+++ control/filter.py
@@ -64,6 +64,31 @@ class KalmanFilter2x:
 
         return dataOut[0][0]
 
+class KalmanFilterNxN:
+    def __init__(self, sigmaX, sigmaXdot, sigmaXdotdot):
+        # Standard deviations
+        self.sigmaX = sigmaX              # Position
+        self.sigmaXdot = sigmaXdot        # Velocity
+        self.sigmaXdotdot = sigmaXdotdot  # Acceleration
+
+        # Configure the filter
+        self.observationModel = np.diag([1, 1, 1, 1])                                                           # H
+        self.observationNoise = np.diag([self.sigmaX**2, self.sigmaXdot**2, self.sigmaX**2, self.sigmaXdot**2]) # R
+
+        # Initial state
+        self.m = np.array([0, 1, 0, 1])
+        self.P = np.eye(4)
+
+    def update(self, dt, dataIn):
+        stateTransition  = np.array([[1, dt, 0, 0], [0, 1, 0, 0], [0, 0, 1, dt], [0, 0, 0, 1]])      # A
+        processNoise = np.diag([0.25*(dt**4), dt**2, 0.25*(dt**4), dt**2]) * self.sigmaXdotdot**2    # Q
+        
+        m, P = update(self.m, self.P, self.observationModel, self.observationNoise, np.array(dataIn))
+        dataOut, _ = predict_observation(m, P, self.observationModel, self.observationNoise)
+        self.m, self.P = predict(m, P, stateTransition, processNoise)
+
+        return dataOut
+    
 class TimeSync:
     def __init__(self, samplingRate):
         self.previousTime = None
