246b08970b22ee800029e0c0f1b4eb4a722db4ec
Mark Sherstan <mshersta@ualberta.ca>
2020-09-11 19:48:42 -0600

    Turn plots into functions

--- control/flightData/plotter.py
+++ control/flightData/plotter.py
@@ -3,6 +3,151 @@ import pandas as pd
 import numpy as np
 import argparse
 
+def General(df, fileName, saveFlag):
+    ########################
+    # Find Autonomous Sections
+    ########################
+    df['modeSwitch'] =  df['Mode'] == 'GUIDED_NOGPS'
+
+    idx = [0]
+    for ii in range(df.shape[0]-1):
+        if (df['modeSwitch'][ii+1] == df['modeSwitch'][ii]):
+            pass
+        else:
+            idx.append(ii)
+
+    ########################
+    # Master
+    ########################
+    fig = plt.figure()
+
+    ########################
+    # Roll and Pitch
+    ########################
+    plt.subplot(2, 2, 1)
+    ax0 = plt.gca()
+
+    df.plot(kind='line', x='Time', y='Roll-UAV',  color='#FB8604', alpha=0.5, style='-',  ax=ax0)
+    df.plot(kind='line', x='Time', y='Pitch-UAV', color='#700CBC', alpha=0.5, style='-',  ax=ax0)
+
+    df.plot(kind='line', x='Time', y='Roll-Control',  color='#FB8604', style='-', ax=ax0)
+    df.plot(kind='line', x='Time', y='Pitch-Control', color='#700CBC', style='-', ax=ax0)
+
+    ax0.set_title('Roll & Pitch Control', fontsize=14, fontweight='bold')
+    ax0.set_xlabel('Time [s]', fontweight='bold')
+    ax0.set_ylabel('Angle [deg]', fontweight='bold')
+
+    for ii in range(0,len(idx),2):
+        plt.axvspan(df['Time'][idx[ii]], df['Time'][idx[ii+1]], color='gray', alpha=0.2)
+        # print(idx[ii], idx[ii+1])
+        # print(df['Time'][idx[ii]], df['Time'][idx[ii+1]])
+
+    ########################
+    # Yaw
+    ########################
+    plt.subplot(2, 2, 2)
+    ax1 = plt.gca()
+
+    df.plot(kind='line', x='Time', y='Yaw-Vision', color='tab:blue', style='-', ax=ax1)
+    df.plot(kind='line', x='Time', y='Yaw-Control', color='tab:blue', style='--', ax=ax1)
+
+    ax1.set_title('Yaw Control', fontsize=14, fontweight='bold')
+    ax1.set_xlabel('Time [s]', fontweight='bold')
+    ax1.set_ylabel('Angle [Deg or Deg/s]', fontweight='bold')
+
+    for ii in range(0,len(idx),2):
+        plt.axvspan(df['Time'][idx[ii]], df['Time'][idx[ii+1]], color='gray', alpha=0.2)
+    
+    ########################
+    # North East
+    ########################
+    plt.subplot(2, 2, 3)
+    ax3 = plt.gca()
+
+    df.plot(kind='line', x='Time', y='North-Vision', color='#700CBC', style='-',  ax=ax3)
+    df.plot(kind='line', x='Time', y='East-Vision',  color='#FB8604', style='-',  ax=ax3)
+    df.plot(kind='line', x='Time', y='Down-Vision',  color='#7FBD32', style='-',  ax=ax3)
+
+    df.plot(kind='line', x='Time', y='North-Desired', color='#700CBC', style='--',  ax=ax3)
+    df.plot(kind='line', x='Time', y='East-Desired',  color='#FB8604', style='--',  ax=ax3)
+    df.plot(kind='line', x='Time', y='Down-Desired',  color='#7FBD32',  style='--',  ax=ax3)
+
+    ax3.set_title('NED Position', fontsize=14, fontweight='bold')
+    ax3.set_xlabel('Time [s]', fontweight='bold')
+    ax3.set_ylabel('Position [cm]', fontweight='bold')
+
+    ax3.legend(['N', 'E', 'D'])
+
+    for ii in range(0,len(idx),2):
+        plt.axvspan(df['Time'][idx[ii]], df['Time'][idx[ii+1]], color='gray', alpha=0.2)
+    
+    ########################
+    # Thrust
+    ########################
+    plt.subplot(2, 2, 4)
+    ax4 = plt.gca()
+
+    df.plot(kind='line', x='Time', y='Thrust-Control', color='#7FBD32', style='-', ax=ax4)
+
+    ax4.set_title('Thrust Control', fontsize=14, fontweight='bold')
+    ax4.set_xlabel('Time [s]', fontweight='bold')
+    ax4.set_ylabel('Normalized Thrust Command', fontweight='bold')
+    ax4.get_legend().remove()
+
+    for ii in range(0,len(idx),2):
+        plt.axvspan(df['Time'][idx[ii]], df['Time'][idx[ii+1]], color='gray', alpha=0.2)
+    
+    ########################
+    # Show the plots
+    ########################
+    # plt.show()
+    
+    if saveFlag is True:
+        fig.savefig(str(fileName).replace('.csv','')+'-GENERAL.png', dpi=fig.dpi)
+
+def FreqSleep(df, fileName, saveFlag): 
+    # Create a figure
+    fig = plt.figure(figsize=(12, 6), dpi=100)
+
+    # Create subplot: Frequency
+    plt.subplot(1, 2, 1)
+    ax1 = plt.gca()
+
+    # Plot data
+    df.plot(kind='line', x='Time', y='Freq',  color='k',  style='-',  ax=ax1)
+
+    # Format figure
+    title = 'Sampling Frequency\n' + '{:<4.3f} +/- {:<0.3f} '.format(df['Freq'].mean(), df['Freq'].std())
+    ax1.set_title(title, fontsize=14, fontweight='bold')
+    ax1.set_xlabel('Time [s]', fontweight='bold')
+    ax1.set_ylabel('Frequency [Hz]', fontweight='bold')
+    ax1.get_legend().remove()
+
+
+    # Create subplot: Sleep request
+    plt.subplot(1, 2, 2)
+    ax0 = plt.gca()
+
+    # Plot data
+    df['time2delay'] = df['time2delay'] * 1000
+    df['actualDelay'] = df['actualDelay'] * 1000
+
+    df.plot(kind='line', x='time2delay', y='actualDelay',  color='r',  style='+',  ax=ax0)
+
+    # Figure formatting
+    ax0.set_title('Sleep Request', fontsize=14, fontweight='bold')
+    ax0.set_xlabel('Requested Sleep Time [ms]', fontweight='bold')
+    ax0.set_ylabel('Actual Sleep Time [ms]', fontweight='bold')
+    ax0.get_legend().remove()
+
+    # Show
+    plt.grid()
+    # plt.show()
+
+    # Save
+    if saveFlag is True:
+        fig.savefig(str(fileName).replace('.csv','')+'-FREQ_SLEEP.png', dpi=fig.dpi)
+
 ########################
 # Argparse
 # Example use: python plotter.py --input "flight1.csv"
@@ -32,142 +177,7 @@ except:
     print('Error with file.')
     exit()
 
-########################
-# Find Autonomous Sections
-########################
-df['modeSwitch'] =  df['Mode'] == 'GUIDED_NOGPS'
-
-idx = [0]
-for ii in range(df.shape[0]-1):
-    if (df['modeSwitch'][ii+1] == df['modeSwitch'][ii]):
-        pass
-    else:
-        idx.append(ii)
-
-########################
-# Master
-########################
-fig = plt.figure()
-
-########################
-# Roll and Pitch
-########################
-plt.subplot(2, 2, 1)
-ax0 = plt.gca()
-
-df.plot(kind='line', x='Time', y='Roll-UAV',  color='#FB8604', alpha=0.5, style='-',  ax=ax0)
-df.plot(kind='line', x='Time', y='Pitch-UAV', color='#700CBC', alpha=0.5, style='-',  ax=ax0)
-
-df.plot(kind='line', x='Time', y='Roll-Control',  color='#FB8604', style='-', ax=ax0)
-df.plot(kind='line', x='Time', y='Pitch-Control', color='#700CBC', style='-', ax=ax0)
-
-ax0.set_title('Roll & Pitch Control', fontsize=14, fontweight='bold')
-ax0.set_xlabel('Time [s]', fontweight='bold')
-ax0.set_ylabel('Angle [deg]', fontweight='bold')
-
-for ii in range(0,len(idx),2):
-    plt.axvspan(df['Time'][idx[ii]], df['Time'][idx[ii+1]], color='gray', alpha=0.2)
-    # print(idx[ii], idx[ii+1])
-    # print(df['Time'][idx[ii]], df['Time'][idx[ii+1]])
-
-########################
-# Yaw
-########################
-plt.subplot(2, 2, 2)
-ax1 = plt.gca()
-
-df.plot(kind='line', x='Time', y='Yaw-Vision', color='tab:blue', style='-', ax=ax1)
-df.plot(kind='line', x='Time', y='Yaw-Control', color='tab:blue', style='--', ax=ax1)
-
-ax1.set_title('Yaw Control', fontsize=14, fontweight='bold')
-ax1.set_xlabel('Time [s]', fontweight='bold')
-ax1.set_ylabel('Angle [Deg or Deg/s]', fontweight='bold')
-
-for ii in range(0,len(idx),2):
-    plt.axvspan(df['Time'][idx[ii]], df['Time'][idx[ii+1]], color='gray', alpha=0.2)
- 
-########################
-# North East
-########################
-plt.subplot(2, 2, 3)
-ax3 = plt.gca()
-
-df.plot(kind='line', x='Time', y='North-Vision', color='#700CBC', style='-',  ax=ax3)
-df.plot(kind='line', x='Time', y='East-Vision',  color='#FB8604', style='-',  ax=ax3)
-df.plot(kind='line', x='Time', y='Down-Vision',  color='#7FBD32', style='-',  ax=ax3)
-
-df.plot(kind='line', x='Time', y='North-Desired', color='#700CBC', style='--',  ax=ax3)
-df.plot(kind='line', x='Time', y='East-Desired',  color='#FB8604', style='--',  ax=ax3)
-df.plot(kind='line', x='Time', y='Down-Desired',  color='#7FBD32',  style='--',  ax=ax3)
-
-ax3.set_title('NED Position', fontsize=14, fontweight='bold')
-ax3.set_xlabel('Time [s]', fontweight='bold')
-ax3.set_ylabel('Position [cm]', fontweight='bold')
-
-ax3.legend(['N', 'E', 'D'])
-
-for ii in range(0,len(idx),2):
-    plt.axvspan(df['Time'][idx[ii]], df['Time'][idx[ii+1]], color='gray', alpha=0.2)
- 
-########################
-# Thrust
-########################
-plt.subplot(2, 2, 4)
-ax4 = plt.gca()
-
-df.plot(kind='line', x='Time', y='Thrust-Control', color='#7FBD32', style='-', ax=ax4)
-
-ax4.set_title('Thrust Control', fontsize=14, fontweight='bold')
-ax4.set_xlabel('Time [s]', fontweight='bold')
-ax4.set_ylabel('Normalized Thrust Command', fontweight='bold')
-ax4.get_legend().remove()
-
-for ii in range(0,len(idx),2):
-    plt.axvspan(df['Time'][idx[ii]], df['Time'][idx[ii+1]], color='gray', alpha=0.2)
- 
-########################
-# Show the plots
-########################
-plt.show()
-fig.savefig(str(fileName).replace('.csv','')+'.png', dpi=fig.dpi)
-
-
-
-
-# Create a figure
-fig = plt.figure(figsize=(12, 6), dpi=100)
-
-# Create subplot: Frequency
-plt.subplot(1, 2, 1)
-ax1 = plt.gca()
-
-# Plot data
-df.plot(kind='line', x='Time', y='Freq',  color='k',  style='-',  ax=ax1)
-
-# Format figure
-title = 'Sampling Frequency\n' + '{:<4.3f} +/- {:<0.3f} '.format(df['Freq'].mean(), df['Freq'].std())
-ax1.set_title(title, fontsize=14, fontweight='bold')
-ax1.set_xlabel('Time [s]', fontweight='bold')
-ax1.set_ylabel('Frequency [Hz]', fontweight='bold')
-ax1.get_legend().remove()
-
-
-# Create subplot: Sleep request
-plt.subplot(1, 2, 2)
-ax0 = plt.gca()
-
-# Plot data
-df['time2delay'] = df['time2delay'] * 1000
-df['actualDelay'] = df['actualDelay'] * 1000
-
-df.plot(kind='line', x='time2delay', y='actualDelay',  color='r',  style='+',  ax=ax0)
-
-# Figure formatting
-ax0.set_title('Sleep Request', fontsize=14, fontweight='bold')
-ax0.set_xlabel('Requested Sleep Time [ms]', fontweight='bold')
-ax0.set_ylabel('Actual Sleep Time [ms]', fontweight='bold')
-ax0.get_legend().remove()
-
-# Show
-plt.grid()
+# Plotting options
+General(df.copy(), fileName, saveFlag=False)
+FreqSleep(df.copy(), fileName, saveFlag=False)
 plt.show()
