620362bf0b906953e4c375a46a32a8de1308c6e0
Mark Sherstan <mshersta@ualberta.ca>
2020-09-23 10:38:06 -0600

    Analysis scripts

--- control/imgs/selectThresh.py
+++ control/imgs/selectThresh.py
@@ -3,73 +3,31 @@ import pandas as pd
 import numpy as np
 import itertools
 
-dataFiles = ['Data0', 'Data1', 'Data2', 'Data3', 'Data4']
-dataList = []
+# Data files 
+dataFiles = ['Data0', 'Data1', 'Data2', 'Data3', 'Data4', 'Data5']
+colors = ['r', 'g', 'b', 'c', 'm', 'k']
 
-for dat in dataFiles:
+# Create a figure 
+fig = plt.figure(figsize=(8, 4), dpi=100)
+ax = plt.gca()
+
+# Loop through results
+for ii in range(len(dataFiles)):
     # Read csv into data frame
-    df = pd.read_csv(dat+'.csv', header=0, names=['adaptiveThreshWinSizeMin', 'adaptiveThreshWinSizeMax',
-                                                'adaptiveThreshWinSizeStep', 'foundA', 'foundB', 'Total Found', 
-                                                'Thresh1', 'Thresh2', 'Cam A Imgs', 'Cam B Imgs'])
+    df = pd.read_csv(dataFiles[ii]+'.csv', header=0, names=['adaptiveThreshWinSizeMin', 'adaptiveThreshWinSizeMax',
+                                        'adaptiveThreshWinSizeStep', 'foundA', 'foundB', 'Total Found', 
+                                        'Thresh', 'Cam A Imgs', 'Cam B Imgs'])
 
     # Create some additional metrics
     df['Percent-Tot'] = (df['foundA'] + df['foundB']) / (df['Cam A Imgs'] + df['Cam B Imgs']) 
-    df['Percent-A'] = df['foundA'] / df['Cam A Imgs'] 
-    df['Percent-B'] = df['foundB'] / df['Cam B Imgs']
-    df['THRESH'] = df.Thresh1.astype(str) + '-' + df.Thresh2.astype(str)
+    
+    # Plot data
+    df.plot(kind='line', x='Thresh', y='Percent-Tot', style='.', color=colors[ii], ax=ax)
 
-    # Find the top 20%
-    top = df.nlargest(int(len(df)*(20/100)), 'Percent-Tot')
+# Format
+ax.set_title('Threshold Vs Accuracy', fontsize=14, fontweight='bold')
+ax.set_xlabel('Threshold Value', fontweight='bold')
+ax.set_ylabel('Relative Accuracy [%]',  fontweight='bold')
+ax.legend(dataFiles)
 
-    dataList.append(top['THRESH'].value_counts()[:10].index.tolist())
-
-
-
-
-
-merged = list(itertools.chain(*dataList))
-A = list(set(merged))
-
-print(A)
-
-# print(df['THRESH'].nunique())
-
-
-# # Time Plot
-# fig = plt.figure(figsize=(8, 4), dpi=100)
-# ax = plt.gca()
-
-# # Plot data
-# df.plot(kind='line', x='THRESH', y='Percent-Tot', style='.', color='k', ax=ax)
-
-# # Format
-# ax.set_title('Test', fontsize=14, fontweight='bold')
-# ax.set_xlabel('Time [s]', fontweight='bold')
-# ax.set_ylabel('dt [s]',  fontweight='bold')
-# ax.get_legend().remove()
-
-
-# plt.show()
-
-
-
-# fig = plt.figure()
-# ax = fig.add_subplot(111, projection='3d')
-
-# x = np.array(df['Thresh1'])
-# y = np.array(df['Thresh2'])
-# z = np.array(df['Percent-Tot'])
-
-# ax.scatter(x, y, z)
-
-# ax.set_xlabel('Thresh 1')
-# ax.set_ylabel('Thresh 2')
-# ax.set_zlabel('Percent Accuracy')
-
-# plt.show()
-
-
-# Plot threshold vs percentage correct?
-# Sort by top camera 1, camera 2, total, and percentages???
-# 3D plot???
-# mode of the top 50 or something?
+plt.show()
---
+++ control/imgs/selectThresh2x.py
@@ -0,0 +1,75 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+import itertools
+
+dataFiles = ['Data0', 'Data1', 'Data2', 'Data3', 'Data4']
+dataList = []
+
+for dat in dataFiles:
+    # Read csv into data frame
+    df = pd.read_csv(dat+'.csv', header=0, names=['adaptiveThreshWinSizeMin', 'adaptiveThreshWinSizeMax',
+                                                'adaptiveThreshWinSizeStep', 'foundA', 'foundB', 'Total Found', 
+                                                'Thresh1', 'Thresh2', 'Cam A Imgs', 'Cam B Imgs'])
+
+    # Create some additional metrics
+    df['Percent-Tot'] = (df['foundA'] + df['foundB']) / (df['Cam A Imgs'] + df['Cam B Imgs']) 
+    df['Percent-A'] = df['foundA'] / df['Cam A Imgs'] 
+    df['Percent-B'] = df['foundB'] / df['Cam B Imgs']
+    df['THRESH'] = df.Thresh1.astype(str) + '-' + df.Thresh2.astype(str)
+
+    # Find the top 20%
+    top = df.nlargest(int(len(df)*(20/100)), 'Percent-Tot')
+
+    dataList.append(top['THRESH'].value_counts()[:10].index.tolist())
+
+
+
+
+
+merged = list(itertools.chain(*dataList))
+A = list(set(merged))
+
+print(A)
+
+# print(df['THRESH'].nunique())
+
+
+# # Time Plot
+# fig = plt.figure(figsize=(8, 4), dpi=100)
+# ax = plt.gca()
+
+# # Plot data
+# df.plot(kind='line', x='THRESH', y='Percent-Tot', style='.', color='k', ax=ax)
+
+# # Format
+# ax.set_title('Test', fontsize=14, fontweight='bold')
+# ax.set_xlabel('Time [s]', fontweight='bold')
+# ax.set_ylabel('dt [s]',  fontweight='bold')
+# ax.get_legend().remove()
+
+
+# plt.show()
+
+
+
+# fig = plt.figure()
+# ax = fig.add_subplot(111, projection='3d')
+
+# x = np.array(df['Thresh1'])
+# y = np.array(df['Thresh2'])
+# z = np.array(df['Percent-Tot'])
+
+# ax.scatter(x, y, z)
+
+# ax.set_xlabel('Thresh 1')
+# ax.set_ylabel('Thresh 2')
+# ax.set_zlabel('Percent Accuracy')
+
+# plt.show()
+
+
+# Plot threshold vs percentage correct?
+# Sort by top camera 1, camera 2, total, and percentages???
+# 3D plot???
+# mode of the top 50 or something?
