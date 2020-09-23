9ca8d3b9a63d753f0cd737e9c18c91e531a51d70
Mark Sherstan <mshersta@ualberta.ca>
2020-09-23 11:47:38 -0600

    Getting there

--- control/imgs/selectThresh2x.py
+++ control/imgs/selectThresh2x.py
@@ -1,14 +1,13 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
-import itertools
 
 dataFiles = ['Data0', 'Data1', 'Data2', 'Data3', 'Data4']
-dataList = []
+dataFrameList = []
 
 for dat in dataFiles:
     # Read csv into data frame
-    df = pd.read_csv(dat+'.csv', header=0, names=['adaptiveThreshWinSizeMin', 'adaptiveThreshWinSizeMax',
+    df = pd.read_csv('2x/'+dat+'.csv', header=0, names=['adaptiveThreshWinSizeMin', 'adaptiveThreshWinSizeMax',
                                                 'adaptiveThreshWinSizeStep', 'foundA', 'foundB', 'Total Found', 
                                                 'Thresh1', 'Thresh2', 'Cam A Imgs', 'Cam B Imgs'])
 
@@ -18,21 +17,54 @@ for dat in dataFiles:
     df['Percent-B'] = df['foundB'] / df['Cam B Imgs']
     df['THRESH'] = df.Thresh1.astype(str) + '-' + df.Thresh2.astype(str)
 
-    # Find the top 20%
-    top = df.nlargest(int(len(df)*(20/100)), 'Percent-Tot')
+    # Store the data
+    dataFrameList.append(df)
+    
+# Combine all data frames into one
+df = pd.concat(dataFrameList)
 
-    dataList.append(top['THRESH'].value_counts()[:10].index.tolist())
+# Average everything based off thresh 1 value
+threshOnePlot = df.groupby('Thresh1').mean()
+
+# Save data into numpy array
+idx = np.array(threshOnePlot.index)
+xxx = np.array(threshOnePlot['Thresh2'])
+yyy = np.array(threshOnePlot['Percent-Tot'])
+
+# Create plot
+fig = plt.figure(figsize=(8, 4), dpi=100)
+ax = plt.gca()
+
+# Plot data
+plt.plot(xxx, yyy)
+# for ii in range(len(idx)):
+#     plt.plot(xxx[ii], yyy[ii], label=idx[ii])
+
+# Format
+ax.set_title('Test', fontsize=14, fontweight='bold')
+ax.set_xlabel('Time [s]', fontweight='bold')
+ax.set_ylabel('dt [s]',  fontweight='bold')
+
+plt.show()
 
 
 
+# temp = df.groupby('THRESH')['Percent-Tot'].mean()
+# print(temp)
+# exit()
+
+# # Find the top 20%
+# top = df.nlargest(int(len(df)*(20/100)), 'Percent-Tot')
+
+# dataList.append(top['THRESH'].value_counts()[:10].index.tolist())
 
 
-merged = list(itertools.chain(*dataList))
-A = list(set(merged))
+# merged = list(itertools.chain(*dataList))
+# A = list(set(merged))
 
-print(A)
+# print(A)
 
-# print(df['THRESH'].nunique())
+# # print(df['THRESH'].nunique())
 
 
 # # Time Plot
@@ -51,6 +83,8 @@ print(A)
 
 # plt.show()
 
+
+# PLOT ONE AS LEGEND OTHER AS RESPONE!!!!!!!!!!!
 
 
 # fig = plt.figure()
