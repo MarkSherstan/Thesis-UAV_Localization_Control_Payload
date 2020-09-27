ea8928bf336c53be8048da89d70775c4a612fd41
MarkSherstan <mshersta@ualberta.ca>
2020-09-26 20:55:59 -0600

    looked gravy:

--- control/controller.py
+++ control/controller.py
@@ -15,12 +15,12 @@ class Controller:
         self.yawRateConstrain = [-10, 10]           # Deg / s
 
         # PID Gains: NORTH (pitch)
-        self.kp_NORTH = 0.03
+        self.kp_NORTH = 0.025
         self.ki_NORTH = 0.00 # Max 1 deg with 500 bounds
         self.kd_NORTH = 0.006
 
         # PID Gains: EAST (roll)
-        self.kp_EAST = 0.03
+        self.kp_EAST = 0.025
         self.ki_EAST = 0.00 # Max 1 deg with 500 bounds
         self.kd_EAST = 0.006
 
