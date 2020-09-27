ca0e9ebc57223be3e0520af11c61e9f3924cf2a6
MarkSherstan <mshersta@ualberta.ca>
2020-09-26 20:35:51 -0600

    That looked pretty good

--- control/controller.py
+++ control/controller.py
@@ -17,12 +17,12 @@ class Controller:
         # PID Gains: NORTH (pitch)
         self.kp_NORTH = 0.03
         self.ki_NORTH = 0.00 # Max 1 deg with 500 bounds
-        self.kd_NORTH = 0.008
+        self.kd_NORTH = 0.007
 
         # PID Gains: EAST (roll)
         self.kp_EAST = 0.03
         self.ki_EAST = 0.00 # Max 1 deg with 500 bounds
-        self.kd_EAST = 0.008
+        self.kd_EAST = 0.007
 
         # PID Gains: DOWN (thrust)
         self.kp_DOWN = 0.002
@@ -30,9 +30,9 @@ class Controller:
         self.kd_DOWN = 0.0
 
         # PID Gains: YAW (yaw rate)
-        self.kp_YAW = 0.5
+        self.kp_YAW = 0.3
         self.ki_YAW = 0
-        self.kd_YAW = 0.1
+        self.kd_YAW = 0
 
         # Cutoff height
         self.gainHeight = 25.0
