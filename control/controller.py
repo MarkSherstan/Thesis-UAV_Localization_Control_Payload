7d3e15e18b9c33df7d5ad9ff5f57863a666643f5
MarkSherstan <mshersta@ualberta.ca>
2020-09-26 20:22:52 -0600

    had some issues with trim on manual control

--- control/controller.py
+++ control/controller.py
@@ -17,12 +17,12 @@ class Controller:
         # PID Gains: NORTH (pitch)
         self.kp_NORTH = 0.03
         self.ki_NORTH = 0.00 # Max 1 deg with 500 bounds
-        self.kd_NORTH = 0.005
+        self.kd_NORTH = 0.01
 
         # PID Gains: EAST (roll)
         self.kp_EAST = 0.03
         self.ki_EAST = 0.00 # Max 1 deg with 500 bounds
-        self.kd_EAST = 0.005
+        self.kd_EAST = 0.01
 
         # PID Gains: DOWN (thrust)
         self.kp_DOWN = 0.002
