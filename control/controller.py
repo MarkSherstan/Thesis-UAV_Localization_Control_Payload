a2200b5677b454f8e519696da258dee20738575d
MarkSherstan <mshersta@ualberta.ca>
2020-09-28 17:39:11 -0600

    This one was pretty good

--- control/controller.py
+++ control/controller.py
@@ -15,14 +15,14 @@ class Controller:
         self.yawRateConstrain = [-10, 10]           # Deg / s
 
         # PID Gains: NORTH (pitch)
-        self.kp_NORTH = 0.02
+        self.kp_NORTH = 0.03
         self.ki_NORTH = 0.005 # Max 2.5 deg with 500 bounds
-        self.kd_NORTH = 0.01
+        self.kd_NORTH = 0.02
 
         # PID Gains: EAST (roll)
-        self.kp_EAST = 0.02
+        self.kp_EAST = 0.03
         self.ki_EAST = 0.005 # Max 2.5 deg with 500 bounds
-        self.kd_EAST = 0.01
+        self.kd_EAST = 0.02
 
         # PID Gains: DOWN (thrust)
         self.kp_DOWN = 0.002
