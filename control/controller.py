f87cbdd94815d29ce45000c193f87fd3d66c1b74
MarkSherstan <mshersta@ualberta.ca>
2020-09-29 13:18:26 -0600

    Keep coming back to these gains

--- control/controller.py
+++ control/controller.py
@@ -19,12 +19,12 @@ class Controller:
         # PID Gains: NORTH (pitch)
         self.kp_NORTH = 0.03
         self.ki_NORTH = 0.005   # Max 2.5 deg with 500 bounds
-        self.kd_NORTH = 0.04
+        self.kd_NORTH = 0.05
 
         # PID Gains: EAST (roll)
         self.kp_EAST = 0.03
         self.ki_EAST = 0.005    # Max 2.5 deg with 500 bounds
-        self.kd_EAST = 0.04
+        self.kd_EAST = 0.05
 
         # PID Gains: DOWN (thrust)
         self.kp_DOWN = 0.002
