85e36e655df84a2de13e2ca3cf9eaacdb3a49c69
MarkSherstan <mshersta@ualberta.ca>
2020-09-28 19:33:46 -0600

    still kinda goes crazy but not bad

--- control/controller.py
+++ control/controller.py
@@ -17,13 +17,13 @@ class Controller:
 
         # PID Gains: NORTH (pitch)
         self.kp_NORTH = 0.03
-        self.ki_NORTH = 0 #0.005   # Max 2.5 deg with 500 bounds
-        self.kd_NORTH = 0.02
+        self.ki_NORTH = 0.005   # Max 2.5 deg with 500 bounds
+        self.kd_NORTH = 0.03
 
         # PID Gains: EAST (roll)
         self.kp_EAST = 0.03
-        self.ki_EAST = 0 #0.005    # Max 2.5 deg with 500 bounds
-        self.kd_EAST = 0.02
+        self.ki_EAST = 0.005    # Max 2.5 deg with 500 bounds
+        self.kd_EAST = 0.03
 
         # PID Gains: DOWN (thrust)
         self.kp_DOWN = 0.002
