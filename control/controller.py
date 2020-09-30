6d098d06ac1a58d54c6d3f6a173e8d85e7bca9b0
MarkSherstan <mshersta@ualberta.ca>
2020-09-30 16:08:48 -0600

    That was a close one

--- control/controller.py
+++ control/controller.py
@@ -24,7 +24,7 @@ class Controller:
         # PID Gains: EAST (roll)
         self.kp_EAST = 0.12 #0.035
         self.ki_EAST = 0.004 #0.005    # Max 2.5 deg with 500 bounds
-        self.kd_EAST = 0.06 #0.05
+        self.kd_EAST = 0.04 #0.05
 
         # PID Gains: DOWN (thrust)
         self.kp_DOWN = 0.002
